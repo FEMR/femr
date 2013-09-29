2553b9dd5f7ab19bf0252a0486e52dfc58aaa8e4
David Palombo <az2324@wayne.edu>
2013-09-29 15:43:50 -0400

    Added to pharmacy layout

---
+++ public/js/pharmacy/pharmacy.js
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: David
+ * Date: 9/29/13
+ * Time: 3:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function yesReplacement(id){
+    $('#replacementMedication' + id ).removeClass('hidden');
+}
+
+function noReplacement(id){
+    $('#replacementMedication' + id ).addClass('hidden');
+}

579704057a83e8e05ad42a0d89df9b56887ca863
Kevin Zurek <kevinzurek@gmail.com>
2013-10-06 13:34:24 -0400

    clean up and track user on show page

--- app/femr/ui/controllers/SearchController.java
+++ app/femr/ui/controllers/SearchController.java
@@ -5,18 +5,15 @@ import femr.business.dtos.CurrentUser;
 import femr.business.dtos.ServiceResponse;
 import femr.business.services.ISearchService;
 import femr.business.services.ISessionService;
-import femr.business.services.SearchService;
 import femr.common.models.IPatient;
 import femr.common.models.IPatientEncounter;
 import femr.ui.models.search.CreateViewModel;
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.util.List;
 
 public class SearchController extends Controller {
-    private final Form<femr.ui.models.triage.CreateViewModel> createViewModelForm = Form.form(femr.ui.models.triage.CreateViewModel.class);
     private ISessionService sessionService;
     private ISearchService searchService;
 
@@ -29,7 +26,7 @@ public class SearchController extends Controller {
 
     public Result createGet(String id){
         IPatient patient = searchService.findPatientById(id).getResponseObject();
-        //ServiceResponse<CurrentUser> currentUserSession = sessionService.getCurrentUserSession();
+        ServiceResponse<CurrentUser> currentUserSession = sessionService.getCurrentUserSession();
         List<? extends IPatientEncounter> patientEncounters = searchService.findAllEncounters();
         CreateViewModel viewModel = new CreateViewModel();
 
@@ -40,9 +37,12 @@ public class SearchController extends Controller {
         viewModel.setAge(patient.getAge());
         viewModel.setSex(patient.getSex());         //awwww yeahhhh!
 
-       // if (currentUserSession.isSuccessful()){
-            return ok(femr.ui.views.html.search.show.render(viewModel, patientEncounters, id));
-        //}
+        if (currentUserSession.isSuccessful()){
+            return ok(femr.ui.views.html.search.show.render(currentUserSession.getResponseObject(),viewModel, patientEncounters, id));
+        }
+        else{
+            return ok(femr.ui.views.html.search.show.render(null,viewModel, patientEncounters, id));
+        }
 
     }
 }

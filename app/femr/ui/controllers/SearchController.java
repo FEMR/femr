07179a0fb4b83bd0a09821ec5374ccd2e538209e
Kevin Zurek <kevinzurek@gmail.com>
2013-10-20 15:28:52 -0400

    adding comments, changing variable names

--- app/femr/ui/controllers/SearchController.java
+++ app/femr/ui/controllers/SearchController.java
@@ -5,21 +5,14 @@ import femr.business.dtos.CurrentUser;
 import femr.business.dtos.ServiceResponse;
 import femr.business.services.ISearchService;
 import femr.business.services.ISessionService;
-import femr.business.services.ITriageService;
 import femr.common.models.IPatient;
 import femr.common.models.IPatientEncounter;
-import femr.common.models.IPatientEncounterVital;
-import femr.common.models.IVital;
 import femr.ui.models.search.CreateViewModel;
-import femr.util.dependencyinjection.providers.PatientEncounterProvider;
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import femr.ui.views.html.search.show;
 import femr.ui.views.html.search.showEncounter;
 import femr.util.stringhelpers.StringUtils;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class SearchController extends Controller {
@@ -45,35 +38,10 @@ public class SearchController extends Controller {
 
     }
 
-    public Result createGet() {
-        CurrentUser currentUser = sessionService.getCurrentUserSession();
-
-        String firstName = request().queryString().get("searchFirstName")[0];
-        String lastName = request().queryString().get("searchLastName")[0];
-        String s_id = request().queryString().get("searchId")[0];
-
-        Integer id = getIdFromSearch(s_id,firstName,lastName);
-        if (id == null)
-            return redirect("triage");
-
-        ServiceResponse<IPatient> patientServiceResponse = searchService.findPatientById(id);
-
-        IPatient patient = patientServiceResponse.getResponseObject();
-
-        List<? extends IPatientEncounter> patientEncounters = searchService.findAllEncountersByPatientId(patient.getId());
-
-        CreateViewModel viewModel = new CreateViewModel();
-        viewModel.setFirstName(patient.getFirstName());
-        viewModel.setLastName(patient.getLastName());
-        viewModel.setAddress(patient.getAddress());
-        viewModel.setCity(patient.getCity());
-        viewModel.setAge(patient.getAge());
-        viewModel.setSex(patient.getSex());
-
-        return ok(show.render(currentUser, viewModel, patientEncounters, patient.getId()));
-    }
-
-    public Result createGetId(int id) {
+    /*
+    GET - detailed patient information
+     */
+    public Result createGet(int id) {
         ServiceResponse<IPatient> patientServiceResponse = searchService.findPatientById(id);
 
         CurrentUser currentUser = sessionService.getCurrentUserSession();
@@ -91,14 +59,31 @@ public class SearchController extends Controller {
             viewModel.setAge(patient.getAge());
             viewModel.setSex(patient.getSex());
         } else {
-            //fail?
+            return redirect("/triage");
         }
 
         return ok(show.render(currentUser, viewModel, patientEncounters, id));
     }
 
+    /*
+    GET - Acquire URL query parameters and createGet()
+     if valid
+     */
+    public Result performSearch() {
+        String firstName = request().queryString().get("searchFirstName")[0];
+        String lastName = request().queryString().get("searchLastName")[0];
+        String s_id = request().queryString().get("searchId")[0];
 
+        Integer id = getIdFromSearch(s_id,firstName,lastName);
+        if (id == null)
+            return redirect("/triage");
+        else
+            return createGet(id);
+    }
 
+    /*
+    Helper for performSearch()
+     */
     private Integer getIdFromSearch(String s_id, String firstName, String lastName){
         Integer id;
         if (StringUtils.isNullOrWhiteSpace(s_id)){
@@ -113,7 +98,6 @@ public class SearchController extends Controller {
         else{
             id = Integer.parseInt(s_id);
         }
-
         return id;
     }
 

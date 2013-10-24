dab517b64c0138c417b7528f265b77dfa2fe0c9d
Kevin Zurek <kevinzurek@gmail.com>
2013-10-24 14:50:01 -0400

    fixed syntax in evolutions 13 and 14

--- conf/evolutions/default/13.sql
+++ conf/evolutions/default/13.sql
@@ -1,10 +1,10 @@
-# --- Ups
+# --- !Ups
 
 ALTER TABLE `patients`
 CHANGE COLUMN `sex` `sex` VARCHAR(10) NULL  ,
 CHANGE COLUMN `address` `address` VARCHAR(255) NULL  ;
 
-# --- Downs
+# --- !Downs
 
 ALTER TABLE `patients`
 CHANGE COLUMN `sex` `sex` VARCHAR(10) NOT NULL  ,

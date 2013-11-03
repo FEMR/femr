808c20878054e4f5d81d3ac16209233364655371
Mrgadgetz <jdub03@gmail.com>
2013-11-03 09:27:54 -0500

    removed autogenerated file header from ageUtils.java

--- app/femr/util/calculations/ageUtils.java
+++ app/femr/util/calculations/ageUtils.java
@@ -6,10 +6,6 @@ import org.joda.time.Years;
 
 import java.util.Date;
 
-/**
- * Author: Mrgadgetz
- * Date:   11/3/13
- */
 public class ageUtils {
     public static int calculateYears(Date age) {
         DateMidnight birthdate = new DateMidnight(age);

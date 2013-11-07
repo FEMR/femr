5c5e159a16cf83d5b31dad7542c9e4df6c233f1a
Kevin Zurek <kevinzurek@gmail.com>
2013-11-07 12:05:31 -0500

    fixing html in pharmacy

--- public/js/pharmacy/pharmacy.js
+++ public/js/pharmacy/pharmacy.js
@@ -1,4 +1,13 @@
 
+$(document).ready(function(){
+    $('.toggleReplacement').click(function(){
+        alert($(this).attr('id'));
+
+        //call yesReplacement or noReplacement based on id
+    });
+
+});
+
 function yesReplacement(id){
     $('#replacementMedication' + id).removeClass('hidden');
     $('#replacementAmount' + id).removeClass('hidden');

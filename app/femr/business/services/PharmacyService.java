929b84ff199696f9ee3f51f7714c47e5b17e2786
Kevin Zurek <kevin.zurek@wayne.edu>
2013-11-10 09:57:22 -0500

    refactor

--- app/femr/business/services/PharmacyService.java
+++ app/femr/business/services/PharmacyService.java
@@ -8,46 +8,40 @@ import com.google.inject.Inject;
 import femr.business.dtos.ServiceResponse;
 import femr.common.models.*;
 import femr.data.daos.IRepository;
-import femr.data.models.Patient;
 import femr.data.models.PatientPrescription;
 
 public class PharmacyService implements IPharmacyService {
     private IRepository<IPatientPrescription> patientPrescriptionRepository;
 
     @Inject
-    public PharmacyService(IRepository<IPatientPrescription> patientPrescriptionRepository){
+    public PharmacyService(IRepository<IPatientPrescription> patientPrescriptionRepository) {
         this.patientPrescriptionRepository = patientPrescriptionRepository;
-
     }
 
     @Override
-    public ServiceResponse<IPatientPrescription> findPatientPrescriptionByEncounterIdAndPrescriptionName(int id, String name){
-
-        ExpressionList<PatientPrescription> query = getPatientPrescriptionQuery().where().eq("encounter_id",id).eq("medication_name",name);
+    public ServiceResponse<IPatientPrescription> findPatientPrescriptionByEncounterIdAndPrescriptionName(int id, String name) {
+        ExpressionList<PatientPrescription> query = getPatientPrescriptionQuery().where().eq("encounter_id", id).eq("medication_name", name);
         IPatientPrescription patientPrescription = patientPrescriptionRepository.findOne(query);
         ServiceResponse<IPatientPrescription> response = new ServiceResponse<>();
-        if (patientPrescription == null){
-            response.addError("patientPrescription","could not find a prescription");
-        }
-        else{
+        if (patientPrescription == null) {
+            response.addError("patientPrescription", "could not find a prescription");
+        } else {
             response.setResponseObject(patientPrescription);
         }
         return response;
     }
 
     @Override
-    public ServiceResponse<IPatientPrescription> updatePatientPrescription(IPatientPrescription patientPrescription){
+    public ServiceResponse<IPatientPrescription> updatePatientPrescription(IPatientPrescription patientPrescription) {
         IPatientPrescription updatedPatientPrescription = patientPrescriptionRepository.update(patientPrescription);
         ServiceResponse<IPatientPrescription> response = new ServiceResponse<>();
-        if (updatedPatientPrescription == null){
-            response.addError("update","update failed");
-        }
-        else{
+        if (updatedPatientPrescription == null) {
+            response.addError("update", "update failed");
+        } else {
             response.setResponseObject(updatedPatientPrescription);
         }
         return response;
     }
-
 
     private Query<PatientPrescription> getPatientPrescriptionQuery() {
         return Ebean.find(PatientPrescription.class);
--- app/femr/business/services/TriageService.java
+++ app/femr/business/services/TriageService.java
@@ -1,38 +1,25 @@
 package femr.business.services;
 
-import com.avaje.ebean.Ebean;
-import com.avaje.ebean.ExpressionList;
-import com.avaje.ebean.Query;
 import com.google.inject.Inject;
 import femr.business.dtos.ServiceResponse;
 import femr.common.models.IPatient;
 import femr.common.models.IPatientEncounter;
 import femr.common.models.IPatientEncounterVital;
-import femr.common.models.IVital;
 import femr.data.daos.IRepository;
-import femr.data.models.Patient;
-import femr.data.models.Vital;
-import femr.data.models.PatientEncounter;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 public class TriageService implements ITriageService {
 
     private IRepository<IPatient> patientRepository;
     private IRepository<IPatientEncounter> patientEncounterRepository;
     private IRepository<IPatientEncounterVital> patientEncounterVitalRepository;
-    private IRepository<IVital> vitalRepository;
 
     @Inject
     public TriageService(IRepository<IPatient> patientRepository,
                          IRepository<IPatientEncounter> patientEncounterRepository,
-                         IRepository<IPatientEncounterVital> patientEncounterVitaRepository,
-                         IRepository<IVital> vitalRepository){
+                         IRepository<IPatientEncounterVital> patientEncounterVitaRepository){
         this.patientRepository = patientRepository;
         this.patientEncounterRepository = patientEncounterRepository;
         this.patientEncounterVitalRepository = patientEncounterVitaRepository;
-        this.vitalRepository = vitalRepository;
     }
 
     @Override

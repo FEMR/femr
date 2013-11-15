ee819d205c93099daf83f7578bbf4fe12ad94c12
JerellDR <jdub03@gmail.com>
2013-11-15 11:52:34 -0500

    [#150] validation for integer values followed by dot error. Limited age values to max 150 years and limited birthdate values to no greater than the current date.

--- public/js/triage/triage.js
+++ public/js/triage/triage.js
@@ -51,20 +51,22 @@ $(document).ready(function () {
         }
 
         // birthdate not null calculate age in years
-        else if ($('#age').val()) {
+        if ($('#age').val()) {
             var birthString = $('#age').val();
             var birthDate = new Date(birthString);
             var today = new Date();
             var currYear = today.getFullYear();
             var birthYear = birthDate.getFullYear();
             var ageInYears = currYear - birthYear;
-            //console.log(ageInYears.valueOf());
+
             // birthdate is not a date clear fields
             var nan = randomString(ageInYears);
-            if (!nan) {
+            if (nan==false) {
                 $('#years').val(ageInYears);
             }
-            else {
+            if (nan == true || birthDate > today){
+                $('#age').val('');
+                $('#years').val('');
                 $('#age').css('border-color', 'red');
                 $('#age').attr('placeholder', 'Enter a date: yyyy-mm-dd')
             }
@@ -73,31 +75,78 @@ $(document).ready(function () {
 
     $('#years').change(function () {
         $('#years').css('border', '');
-        var yrs = $('#years').val();
         // years in age not null calculate birthdate
-        if (yrs) {
+        if ($('#years').val()) {
+            var yrs = $('#years').val();
             var birthDate = new Date();
             birthDate.setFullYear(birthDate.getFullYear() - yrs);
             var birthString = birthDate.toYMD();
             var nan = randomString(birthDate);
-
-            if (!nan) {
+            var checkDate = new Date();
+            if (nan==false ) {
                 $('#age').val(birthString);
             }
-            if (nan || !(yrs > -1)) {
-                $('#years').val('');
-                $('#age').val('');
+            if (nan==true || parseInt(yrs) < 1 || parseInt(yrs) > 150) {
+                $('#years').val("");
+                $('#age').val("");
                 $('#years').css('border-color', 'red');
                 $('#years').attr('placeholder', 'Enter correct age in Years');
             }
         }
         // age in years erased, clear birthdate field
-        else if (!$('#years').val()) {
+        if (!$('#years').val()) {
             $('#age').val('');
         }
     });
 });
 
+//BMI auto- calculator
+window.setInterval(function () {
+    if ($('#heightFeet').val() && $('#weight').val() && $('#heightInches').val()) {
+
+        var weight_lbs = parseInt($('#weight').val());
+        var height_in = parseInt($('#heightInches').val());
+        var height_ft = parseInt($('#heightFeet').val());
+
+        height_in = height_in + height_ft * 12;
+
+        $('#bmi').val(Math.round((weight_lbs / (height_in * height_in)) * 703));
+
+    }
+}, 500);
+
+//Datepicker select age
+
+
+(function () {
+    // Format date object as yyyy-MM-dd
+    Date.prototype.toYMD = Date_toYMD;
+    function Date_toYMD() {
+        var year, month, day;
+        year = String(this.getFullYear());
+        month = String(this.getMonth() + 1);
+        if (month.length == 1) {
+            month = "0" + month;
+        }
+        day = String(this.getDate());
+        if (day.length == 1) {
+            day = "0" + day;
+        }
+        return year + "-" + month + "-" + day;
+    }
+})();
+
+function randomString(strVal) {
+    if (isNaN(strVal)) {
+        $('#age').val('');
+        $('#years').val('');
+        return true;
+    }
+    return false;
+}
+
+
+ // reset border colors on input change
 $('#firstName').change(function () {
     $('#firstName').css('border', '');
 });

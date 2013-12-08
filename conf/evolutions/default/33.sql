eaf73452b8c336666c72ed25205d8e1a95ae7aa5
Kevin Zurek <kevinzurek@gmail.com>
2013-12-08 11:22:24 -0500

    changed order of evolution 33 query to delete fk reference before deleting primary key

--- conf/evolutions/default/33.sql
+++ conf/evolutions/default/33.sql
@@ -1,10 +1,12 @@
 # --- !Ups
-INSERT INTO `vitals` (`id`, `name`, `data_type`, `unit_of_measurement`) VALUES ('10', 'glucose', 'int', 'mg/dl');
+INSERT INTO `vitals` (`id`, `name`, `data_type`, `unit_of_measurement`)
+  VALUES ('10', 'glucose', 'int', 'mg/dl');
 
 # --- !Downs
 
-DELETE FROM `vitals`
-WHERE id=10;
 DELETE FROM `patient_encounter_vitals`
 WHERE vital_id=10;
+DELETE FROM `vitals`
+WHERE id=10;
+
 

aa006b6ffbdbe325d9e426c693d3c67aa96812be
codeitandloadit <reinhei2@wayne.edu>
2014-02-03 12:24:49 -0500

    Updated the search controller to populate the medical date Updated the encounter page to show the medical date in readonly fields created a ouput file to display the list of prescriptions or problems

--- app/femr/ui/controllers/SearchController.java
+++ app/femr/ui/controllers/SearchController.java
@@ -5,7 +5,9 @@ import femr.business.dtos.CurrentUser;
 import femr.business.dtos.ServiceResponse;
 import femr.business.services.ISearchService;
 import femr.business.services.ISessionService;
+import femr.business.services.IMedicalService;
 import femr.common.models.*;
+import femr.ui.helpers.controller.MedicalHelper;
 import femr.ui.models.search.CreateEncounterViewModel;
 import femr.ui.models.search.CreateViewModel;
 import femr.util.calculations.dateUtils;
@@ -17,7 +19,9 @@ import femr.ui.views.html.search.showError;
 import femr.util.stringhelpers.StringUtils;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 // include the view model for medical
 import femr.ui.models.medical.CreateViewModelGet;
@@ -28,12 +32,17 @@ import femr.ui.models.medical.CreateViewModelGet;
 public class SearchController extends Controller {
     private ISessionService sessionService;
     private ISearchService searchService;
-
+    private IMedicalService medicalService;
+    private MedicalHelper medicalHelper;
     @Inject
     public SearchController(ISessionService sessionService,
-                            ISearchService searchService) {
+                            ISearchService searchService,
+                            IMedicalService medicalService,
+                            MedicalHelper medicalHelper) {
         this.sessionService = sessionService;
         this.searchService = searchService;
+        this.medicalService = medicalService;
+        this.medicalHelper = medicalHelper;
     }
 
     /*
@@ -46,6 +55,8 @@ public class SearchController extends Controller {
         CurrentUser currentUser = sessionService.getCurrentUserSession();
         ServiceResponse<IPatientEncounter> patientEncounterServiceResponse = searchService.findPatientEncounterById(id);
         IPatientEncounter patientEncounter = patientEncounterServiceResponse.getResponseObject();
+
+
 
 
         //findPatientEncounterVitalByVitalIdAndEncounterId() is in the process of being replaced
@@ -105,22 +116,118 @@ public class SearchController extends Controller {
         //Get Patient Name and other basic info
         ServiceResponse<IPatient> patientServiceResponseid = null;
         patientServiceResponseid = searchService.findPatientById(patientEncounter.getPatientId());
+        IPatient patient = patientServiceResponseid.getResponseObject();
         if (!patientServiceResponseid.hasErrors()) {
-            IPatient patient = patientServiceResponseid.getResponseObject();
+
             viewModel.setFirstName(patient.getFirstName());
             viewModel.setLastName(patient.getLastName());
             viewModel.setAddress(patient.getAddress());
             viewModel.setCity(patient.getCity());
             viewModel.setAge(dateUtils.calculateYears(patient.getAge()));
             viewModel.setSex(patient.getSex());
+
         }
+
+        // Get medical
+
+
+        // Add the Medical data
+        //get a list of prescriptions
+        ServiceResponse<List<? extends IPatientPrescription>> patientPrescriptionsServiceResponse = searchService.findPrescriptionsByEncounterId(patientEncounter.getId());
+        List<? extends IPatientPrescription> patientPrescriptions = new ArrayList<>();
+        if (patientPrescriptionsServiceResponse.hasErrors()) {
+            //do nothing, there might not always be available prescriptions
+        } else {
+            patientPrescriptions = patientPrescriptionsServiceResponse.getResponseObject();
+        }
+
+        //region **Mapping of treatment fields, HPI, PMH, and vitals**
+
+        //Create linked hash map of treatment fields
+        //get a list of available treatment fields
+        ServiceResponse<List<? extends ITreatmentField>> treatmentFieldsServiceResponse = searchService.findAllTreatmentFields();
+        List<? extends ITreatmentField> treatmentFields = treatmentFieldsServiceResponse.getResponseObject();
+
+        //initalize map to store treatment fields: Map<treatmentFieldName, List of values>
+        //the list of values is stored in desecending order by time taken
+        Map<String, List<? extends IPatientEncounterTreatmentField>> patientEncounterTreatmentMap = new LinkedHashMap<>();
+        ServiceResponse<List<? extends IPatientEncounterTreatmentField>> patientTreatmentServiceResponse;
+        String treatmentFieldName;
+        //loop through each available treatment field and build the map
+        for (int treatmentFieldIndex = 0; treatmentFieldIndex < treatmentFields.size(); treatmentFieldIndex++) {
+            treatmentFieldName = treatmentFields.get(treatmentFieldIndex).getName().trim();
+            patientTreatmentServiceResponse = searchService.findTreatmentFields(patientEncounter.getId(), treatmentFieldName);
+            if (patientTreatmentServiceResponse.hasErrors()) {
+                continue;
+            } else {
+                patientEncounterTreatmentMap.put(treatmentFieldName, patientTreatmentServiceResponse.getResponseObject());
+            }
+        }
+
+        //Create linked hash map of history of present illness fields
+        ServiceResponse<List<? extends IHpiField>> hpiFieldServiceResponse = searchService.findAllHpiFields();
+        List<? extends IHpiField> hpiFields = hpiFieldServiceResponse.getResponseObject();
+
+        Map<String, List<? extends IPatientEncounterHpiField>> patientEncounterHpiMap = new LinkedHashMap<>();
+        ServiceResponse<List<? extends IPatientEncounterHpiField>> patientHpiServiceResponse;
+        String hpiFieldName;
+        for (int hpiFieldIndex = 0; hpiFieldIndex < hpiFields.size(); hpiFieldIndex++) {
+            hpiFieldName = hpiFields.get(hpiFieldIndex).getName().trim();
+            patientHpiServiceResponse = searchService.findHpiFields(patientEncounter.getId(), hpiFieldName);
+            if (patientHpiServiceResponse.hasErrors()) {
+                continue;
+            } else {
+                patientEncounterHpiMap.put(hpiFieldName, patientHpiServiceResponse.getResponseObject());
+            }
+        }
+
+        //Create linked hash map of past medical history fields
+        ServiceResponse<List<? extends IPmhField>> pmhFieldServiceResponse = searchService.findAllPmhFields();
+        List<? extends IPmhField> pmhFields = pmhFieldServiceResponse.getResponseObject();
+
+        Map<String, List<? extends IPatientEncounterPmhField>> patientEncounterPmhMap = new LinkedHashMap<>();
+        ServiceResponse<List<? extends IPatientEncounterPmhField>> patientPmhServiceResponse;
+        String pmhFieldName;
+        for (int pmhFieldIndex = 0; pmhFieldIndex < pmhFields.size(); pmhFieldIndex++) {
+            pmhFieldName = pmhFields.get(pmhFieldIndex).getName().trim();
+            patientPmhServiceResponse = searchService.findPmhFields(patientEncounter.getId(), pmhFieldName);
+            if (patientPmhServiceResponse.hasErrors()) {
+                continue;
+            } else {
+                patientEncounterPmhMap.put(pmhFieldName, patientPmhServiceResponse.getResponseObject());
+            }
+        }
+
+        //Create linked hash map of vitals
+        ServiceResponse<List<? extends IVital>> vitalServiceResponse = searchService.findAllVitals();
+        List<? extends IVital> vitals = vitalServiceResponse.getResponseObject();
+
+        Map<String, List<? extends IPatientEncounterVital>> patientEncounterVitalMap = new LinkedHashMap<>();
+        ServiceResponse<List<? extends IPatientEncounterVital>> patientVitalServiceResponse;
+        String vitalFieldName;
+        for (int vitalFieldIndex = 0; vitalFieldIndex < vitals.size(); vitalFieldIndex++) {
+            vitalFieldName = vitals.get(vitalFieldIndex).getName().trim();
+            patientVitalServiceResponse = searchService.findPatientEncounterVitals(patientEncounter.getId(), vitalFieldName);
+            if (patientVitalServiceResponse.hasErrors()) {
+                continue;
+            } else {
+                patientEncounterVitalMap.put(vitalFieldName, patientVitalServiceResponse.getResponseObject());
+            }
+        }
+
+        //endregion
+
+        //set up viewModelGet with everything except vitals
+        femr.ui.models.medical.CreateViewModelGet viewMedicalModel = medicalHelper.populateViewModelGet(patient, patientEncounter, patientPrescriptions, patientEncounterVitalMap, patientEncounterTreatmentMap, patientEncounterHpiMap, patientEncounterPmhMap);
+
+        // End of medical data
 
         // Get the medical Responce
         // Store it in the medical component
 
-        femr.ui.models.medical.CreateViewModelGet MedicalModel = new femr.ui.models.medical.CreateViewModelGet();
+
         femr.ui.models.pharmacy.CreateViewModelGet PharmacyModel = new femr.ui.models.pharmacy.CreateViewModelGet();
-        viewModel.setMedicalView(MedicalModel);
+        viewModel.setMedicalView(viewMedicalModel);
         viewModel.setPharmacyView(PharmacyModel);
 
         /*                findAllTreatmentByEncounterId is in the process of being replaced
@@ -148,7 +255,7 @@ public class SearchController extends Controller {
                                 */
         //Get patient prescriptions
         List<String> prescriptionsList = new ArrayList<String>();
-        ServiceResponse<List<? extends IPatientPrescription>> patientPrescriptionsServiceResponse = searchService.findPrescriptionsByEncounterId(patientEncounter.getId());
+        //ServiceResponse<List<? extends IPatientPrescription>> patientPrescriptionsServiceResponse = searchService.findPrescriptionsByEncounterId(patientEncounter.getId());
         if (!patientPrescriptionsServiceResponse.hasErrors()) {
             for (int i = 0; i < patientPrescriptionsServiceResponse.getResponseObject().size(); i++) {
                 prescriptionsList.add(patientPrescriptionsServiceResponse.getResponseObject().get(i).getMedicationName());

003dc06cfa851d1a1af2b6b095613c1434407056
codeitandloadit <reinhei2@wayne.edu>
2014-02-07 10:20:30 -0500

    Refactored so that the encounter class is 1 to 1 and not storing medical in side of it.  created an incounter helper thats a duplicate of medical helper. refactered the encounter page so it uses only one model.

---
+++ app/femr/ui/helpers/controller/EncounterHelper.java
@@ -0,0 +1,218 @@
+package femr.ui.helpers.controller;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import femr.business.dtos.CurrentUser;
+import femr.common.models.*;
+import femr.ui.models.search.CreateEncounterViewModel;
+import femr.util.calculations.dateUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class EncounterHelper {
+    private Provider<IPatientEncounterTreatmentField> patientEncounterTreatmentFieldProvider;
+    private Provider<IPatientEncounterHpiField> patientEncounterHpiFieldProvider;
+    private Provider<IPatientEncounterPmhField> patientEncounterPmhFieldProvider;
+    private Provider<IPatientPrescription> patientPrescriptionProvider;
+    private Provider<IPatientEncounterVital> patientEncounterVitalProvider;
+
+    @Inject
+    public EncounterHelper(Provider<IPatientEncounterTreatmentField> patientEncounterTreatmentFieldProvider, Provider<IPatientEncounterHpiField> patientEncounterHpiFieldProvider, Provider<IPatientEncounterPmhField> patientEncounterPmhFieldProvider, Provider<IPatientPrescription> patientPrescriptionProvider, Provider<IPatientEncounterVital> patientEncounterVitalProvider) {
+        this.patientEncounterTreatmentFieldProvider = patientEncounterTreatmentFieldProvider;
+        this.patientEncounterHpiFieldProvider = patientEncounterHpiFieldProvider;
+        this.patientEncounterPmhFieldProvider = patientEncounterPmhFieldProvider;
+        this.patientEncounterVitalProvider = patientEncounterVitalProvider;
+        this.patientPrescriptionProvider = patientPrescriptionProvider;
+    }
+
+    public CreateEncounterViewModel populateViewModelGet(IPatient patient, IPatientEncounter patientEncounter, List<? extends IPatientPrescription> patientPrescriptions, Map<String, List<? extends IPatientEncounterVital>> patientEncounterVitalMap, Map<String, List<? extends IPatientEncounterTreatmentField>> patientEncounterTreatmentMap, Map<String, List<? extends IPatientEncounterHpiField>> patientEncounterHpiMap, Map<String, List<? extends IPatientEncounterPmhField>> patientEncounterPmhMap) {
+        CreateEncounterViewModel viewModelGet = new CreateEncounterViewModel();
+        //prescriptions
+        List<String> dynamicViewMedications = new ArrayList<>();
+        for (int filledPrescription = 0; filledPrescription < patientPrescriptions.size(); filledPrescription++) {
+            if (patientPrescriptions.get(filledPrescription).getReplaced() != true) {
+                dynamicViewMedications.add(patientPrescriptions.get(filledPrescription).getMedicationName());
+            }
+        }
+
+        //patient
+        viewModelGet.setpID(patient.getId());
+        viewModelGet.setCity(patient.getCity());
+        viewModelGet.setFirstName(patient.getFirstName());
+        viewModelGet.setLastName(patient.getLastName());
+        viewModelGet.setAge(dateUtils.calculateYears(patient.getAge()));
+        viewModelGet.setSex(patient.getSex());
+        //patient encounter
+        viewModelGet.setChiefComplaint(patientEncounter.getChiefComplaint());
+        viewModelGet.setWeeksPregnant(patientEncounter.getWeeksPregnant());
+        //editable information - prescriptions
+        viewModelGet.setPrescription1(getPrescriptionOrNull(1, dynamicViewMedications));
+        viewModelGet.setPrescription2(getPrescriptionOrNull(2, dynamicViewMedications));
+        viewModelGet.setPrescription3(getPrescriptionOrNull(3, dynamicViewMedications));
+        viewModelGet.setPrescription4(getPrescriptionOrNull(4, dynamicViewMedications));
+        viewModelGet.setPrescription5(getPrescriptionOrNull(5, dynamicViewMedications));
+        //editable information - Treatment_fields
+        viewModelGet.setAssessment(getTreatmentFieldOrNull("assessment", patientEncounterTreatmentMap));
+        viewModelGet.setProblem1(getTreatmentProblemOrNull(1, patientEncounterTreatmentMap));
+        viewModelGet.setProblem2(getTreatmentProblemOrNull(2, patientEncounterTreatmentMap));
+        viewModelGet.setProblem3(getTreatmentProblemOrNull(3, patientEncounterTreatmentMap));
+        viewModelGet.setProblem4(getTreatmentProblemOrNull(4, patientEncounterTreatmentMap));
+        viewModelGet.setProblem5(getTreatmentProblemOrNull(5, patientEncounterTreatmentMap));
+        viewModelGet.setTreatment(getTreatmentFieldOrNull("treatment", patientEncounterTreatmentMap));
+        viewModelGet.setFamilyHistory(getTreatmentFieldOrNull("familyHistory", patientEncounterTreatmentMap));
+        //editable information - Hpi_fields
+        viewModelGet.setOnset(getHpiFieldOrNull("onset", patientEncounterHpiMap));
+        viewModelGet.setSeverity(getHpiFieldOrNull("severity", patientEncounterHpiMap));
+        viewModelGet.setRadiation(getHpiFieldOrNull("radiation", patientEncounterHpiMap));
+        viewModelGet.setQuality(getHpiFieldOrNull("quality", patientEncounterHpiMap));
+        viewModelGet.setProvokes(getHpiFieldOrNull("provokes", patientEncounterHpiMap));
+        viewModelGet.setPalliates(getHpiFieldOrNull("palliates", patientEncounterHpiMap));
+        viewModelGet.setTimeOfDay(getHpiFieldOrNull("timeOfDay", patientEncounterHpiMap));
+        viewModelGet.setPhysicalExamination(getHpiFieldOrNull("physicalExamination", patientEncounterHpiMap));
+        viewModelGet.setNarrative(getHpiFieldOrNull("narrative", patientEncounterHpiMap));
+        //editable information - Pmh_fields
+        viewModelGet.setMedicalSurgicalHistory(getPmhFieldOrNull("medicalSurgicalHistory", patientEncounterPmhMap));
+        viewModelGet.setSocialHistory(getPmhFieldOrNull("socialHistory", patientEncounterPmhMap));
+        viewModelGet.setCurrentMedication(getPmhFieldOrNull("currentMedication", patientEncounterPmhMap));
+        viewModelGet.setFamilyHistory(getPmhFieldOrNull("familyHistory", patientEncounterPmhMap));
+        //vitals
+        viewModelGet.setRespiratoryRate(getIntVitalOrNull("respiratoryRate", patientEncounterVitalMap));
+        viewModelGet.setHeartRate(getIntVitalOrNull("heartRate", patientEncounterVitalMap));
+        viewModelGet.setHeightFeet(getIntVitalOrNull("heightFeet", patientEncounterVitalMap));
+        viewModelGet.setHeightInches(getIntVitalOrNull("heightInches", patientEncounterVitalMap));
+        viewModelGet.setBloodPressureSystolic(getIntVitalOrNull("bloodPressureSystolic", patientEncounterVitalMap));
+        viewModelGet.setBloodPressureDiastolic(getIntVitalOrNull("bloodPressureDiastolic", patientEncounterVitalMap));
+        viewModelGet.setTemperature(getFloatVitalOrNull("temperature", patientEncounterVitalMap));
+        viewModelGet.setOxygenSaturation(getFloatVitalOrNull("oxygenSaturation", patientEncounterVitalMap));
+        viewModelGet.setWeight(getFloatVitalOrNull("weight", patientEncounterVitalMap));
+        viewModelGet.setGlucose(getFloatVitalOrNull("glucose", patientEncounterVitalMap));
+
+        return viewModelGet;
+    }
+
+    //region **get encounter fields**
+    public IPatientPrescription getPatientPrescription(int userId, int patientEncounterId, String name){
+        IPatientPrescription patientPrescription = patientPrescriptionProvider.get();
+        patientPrescription.setEncounterId(patientEncounterId);
+        patientPrescription.setUserId(userId);
+        patientPrescription.setReplaced(false);
+        patientPrescription.setReplacementId(null);
+        patientPrescription.setDateTaken(dateUtils.getCurrentDateTime());
+        patientPrescription.setMedicationName(name);
+        return patientPrescription;
+    }
+    public IPatientEncounterVital getPatientEncounterVital(int userId, int patientEncounterId, IVital vital, double vitalValue) {
+        IPatientEncounterVital patientEncounterVital = patientEncounterVitalProvider.get();
+        patientEncounterVital.setUserId(userId);
+        patientEncounterVital.setPatientEncounterId(patientEncounterId);
+        patientEncounterVital.setVital(vital);
+        patientEncounterVital.setVitalValue((float) vitalValue);
+        patientEncounterVital.setDateTaken(dateUtils.getCurrentDateTimeString());
+        return patientEncounterVital;
+    }
+
+    public IPatientEncounterTreatmentField getPatientEncounterTreatmentField(int userId, int patientEncounterId, ITreatmentField treatmentField, String treatmentValue) {
+        IPatientEncounterTreatmentField patientEncounterTreatmentField = patientEncounterTreatmentFieldProvider.get();
+        patientEncounterTreatmentField.setUserId(userId);
+        patientEncounterTreatmentField.setPatientEncounterId(patientEncounterId);
+        patientEncounterTreatmentField.setTreatmentField(treatmentField);
+        patientEncounterTreatmentField.setTreatmentFieldValue(treatmentValue.trim());
+        patientEncounterTreatmentField.setDateTaken(dateUtils.getCurrentDateTime());
+        return patientEncounterTreatmentField;
+    }
+
+    public IPatientEncounterPmhField getPatientEncounterPmhField(int userId, int patientEncounterId, IPmhField pmhField, String pmhValue) {
+        IPatientEncounterPmhField patientEncounterPmhField = patientEncounterPmhFieldProvider.get();
+        patientEncounterPmhField.setUserId(userId);
+        patientEncounterPmhField.setPatientEncounterId(patientEncounterId);
+        patientEncounterPmhField.setPmhField(pmhField);
+        patientEncounterPmhField.setPmhFieldValue(pmhValue.trim());
+        patientEncounterPmhField.setDateTaken(dateUtils.getCurrentDateTime());
+        return patientEncounterPmhField;
+    }
+
+    public IPatientEncounterHpiField getPatientEncounterHpiField(int userId, int patientEncounterId, IHpiField hpiField, String hpiValue) {
+        IPatientEncounterHpiField patientEncounterHpiField = patientEncounterHpiFieldProvider.get();
+        patientEncounterHpiField.setUserId(userId);
+        patientEncounterHpiField.setPatientEncounterId(patientEncounterId);
+        patientEncounterHpiField.setHpiField(hpiField);
+        patientEncounterHpiField.setHpiFieldValue(hpiValue);
+        patientEncounterHpiField.setDateTaken(dateUtils.getCurrentDateTime());
+        return patientEncounterHpiField;
+    }
+    //endregion
+
+    //region **get value or get null**
+    private String getPrescriptionOrNull(int number, List<String> patientPrescriptions) {
+        if (patientPrescriptions.size() >= number) {
+            return patientPrescriptions.get(number - 1);
+        } else {
+            return null;
+        }
+    }
+
+    private Integer getIntVitalOrNull(String key, Map<String, List<? extends IPatientEncounterVital>> patientEncounterVitalMap) {
+        if (patientEncounterVitalMap.containsKey(key)) {
+            if (patientEncounterVitalMap.get(key).size() < 1) {
+                return null;
+            }
+            return patientEncounterVitalMap.get(key).get(0).getVitalValue().intValue();
+        }
+        return null;
+    }
+
+    private Float getFloatVitalOrNull(String key, Map<String, List<? extends IPatientEncounterVital>> patientEncounterVitalMap) {
+        if (patientEncounterVitalMap.containsKey(key)) {
+            if (patientEncounterVitalMap.get(key).size() < 1) {
+                return null;
+            }
+            return patientEncounterVitalMap.get(key).get(0).getVitalValue();
+        }
+        return null;
+    }
+
+    private String getHpiFieldOrNull(String key, Map<String, List<? extends IPatientEncounterHpiField>> patientEncounterHpiMap) {
+        if (patientEncounterHpiMap.containsKey(key)) {
+            if (patientEncounterHpiMap.get(key).size() < 1) {
+                return null;
+            }
+            return patientEncounterHpiMap.get(key).get(0).getHpiFieldValue().trim();
+        }
+        return null;
+    }
+
+    private String getPmhFieldOrNull(String key, Map<String, List<? extends IPatientEncounterPmhField>> patientEncounterPmhMap) {
+        if (patientEncounterPmhMap.containsKey(key)) {
+            if (patientEncounterPmhMap.get(key).size() < 1) {
+                return null;
+            }
+            return patientEncounterPmhMap.get(key).get(0).getPmhFieldValue().trim();
+        }
+        return null;
+    }
+
+
+    private String getTreatmentFieldOrNull(String key, Map<String, List<? extends IPatientEncounterTreatmentField>> patientEncounterTreatmentMap) {
+        if (patientEncounterTreatmentMap.containsKey(key)) {
+            if (patientEncounterTreatmentMap.get(key).size() < 1) {
+                return null;
+            }
+            return patientEncounterTreatmentMap.get(key).get(0).getTreatmentFieldValue().trim();
+        }
+        return null;
+    }
+
+    private String getTreatmentProblemOrNull(int problem, Map<String, List<? extends IPatientEncounterTreatmentField>> patientEncounterTreatmentMap) {
+        if (patientEncounterTreatmentMap.containsKey("problem")) {
+            if (patientEncounterTreatmentMap.get("problem").size() >= problem) {
+                return patientEncounterTreatmentMap.get("problem").get(problem - 1).getTreatmentFieldValue();
+            }
+            return null;
+        }
+        return null;
+    }
+    //endregion
+}
--- app/femr/ui/models/search/CreateEncounterViewModel.java
+++ app/femr/ui/models/search/CreateEncounterViewModel.java
@@ -1,12 +1,11 @@
 package femr.ui.models.search;
 
 import java.util.List;
-//import femr.ui.models.medical.CreateViewModelGet;
-//import femr.ui.models.pharmacy.CreateViewModelGet;
+
 
 
 public class CreateEncounterViewModel {
-    private Integer bloodPressureSystolic;
+   /* private Integer bloodPressureSystolic;
     private Integer bloodPressureDiastolic;
     private Integer heartRate;
     private Float temperature;
@@ -17,36 +16,443 @@ public class CreateEncounterViewModel {
     private Float weight;
     private Float glucose;
     private String firstName;
-    private String lastName;
+    private String lastName;*/
     private String address;
-    private String city;
+/*    private String city;
     private int age;
     private String sex;
     private String assessment;
-    private String treatment;
+    private String treatment;*/
     private String familyHist;
     private List<String> problemList;
     private List<String> perscribList;
 
+    // Copied the properties and methods from medical model
+    // Bad practice but not my design
 
-    // Add view models to this using component
-    private femr.ui.models.medical.CreateViewModelGet MedicalView;
+    //begin patient
+    private int pID;
+    private String city;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String sex;
+    //begin vitals
+    private Integer bloodPressureSystolic;
+    private Integer bloodPressureDiastolic;
+    private Integer heartRate;
+    private Float temperature;
+    private Integer respiratoryRate;
+    private Float oxygenSaturation;
+    private Integer heightFeet;
+    private Integer heightInches;
+    private Float weight;
+    private Float glucose;
+    //begin encounter
+    private String chiefComplaint;
+    private Integer weeksPregnant;
+    //editable information - prescriptions
+    private String prescription1;
+    private String prescription2;
+    private String prescription3;
+    private String prescription4;
+    private String prescription5;
+    //editable information - Treatment_fields
+    private String assessment;
+    private String problem1;
+    private String problem2;
+    private String problem3;
+    private String problem4;
+    private String problem5;
+    private String treatment;
+    //editable information - HPI_fields
+    private String onset;
+    private String severity;
+    private String radiation;
+    private String quality;
+    private String provokes;
+    private String palliates;
+    private String timeOfDay;
+    private String physicalExamination;
+    private String narrative;
+    //editable information - PMH
+    private String familyHistory;
+    private String medicalSurgicalHistory;
+    private String socialHistory;
+    private String currentMedication;
+    // Pharmacy information
+    //Prescriptions
+    private String[] medications;
 
-    private femr.ui.models.pharmacy.CreateViewModelGet PharmacyView;
 
+    public int getpID() {
+        return pID;
+    }
 
+    public void setpID(int pID) {
+        this.pID = pID;
+    }
 
-    // Medical View getter and setter
-    public femr.ui.models.medical.CreateViewModelGet getMedicalView() { return MedicalView; }
+    public String getCity() {
+        return city;
+    }
 
-    public void setMedicalView(femr.ui.models.medical.CreateViewModelGet medicalView) { MedicalView = medicalView; }
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-    // Pharmacy view getter and setter
-    public femr.ui.models.pharmacy.CreateViewModelGet getPharmacyView() { return PharmacyView; }
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public void setPharmacyView(femr.ui.models.pharmacy.CreateViewModelGet pharmacyView) { PharmacyView = pharmacyView; }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     public Integer getBloodPressureSystolic() {
+        return bloodPressureSystolic;
+    }
+
+    public void setBloodPressureSystolic(Integer bloodPressureSystolic) {
+        this.bloodPressureSystolic = bloodPressureSystolic;
+    }
+
+    public Integer getBloodPressureDiastolic() {
+        return bloodPressureDiastolic;
+    }
+
+    public void setBloodPressureDiastolic(Integer bloodPressureDiastolic) {
+        this.bloodPressureDiastolic = bloodPressureDiastolic;
+    }
+
+    public Integer getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(Integer heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public Float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Float temperature) {
+        this.temperature = temperature;
+    }
+
+    public Float getOxygenSaturation() {
+        return oxygenSaturation;
+    }
+
+    public void setOxygenSaturation(Float oxygenSaturation) {
+        this.oxygenSaturation = oxygenSaturation;
+    }
+
+    public Integer getRespiratoryRate() {
+        return respiratoryRate;
+    }
+
+    public void setRespiratoryRate(Integer respiratoryRate) {
+        this.respiratoryRate = respiratoryRate;
+    }
+
+    public Integer getHeightFeet() {
+        return heightFeet;
+    }
+
+    public void setHeightFeet(Integer heightFeet) {
+        this.heightFeet = heightFeet;
+    }
+
+    public Integer getHeightInches() {
+        return heightInches;
+    }
+
+    public void setHeightInches(Integer heightInches) {
+        this.heightInches = heightInches;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public String getChiefComplaint() {
+        return chiefComplaint;
+    }
+
+    public void setChiefComplaint(String chiefComplaint) {
+        this.chiefComplaint = chiefComplaint;
+    }
+
+    public Integer getWeeksPregnant() {
+        return weeksPregnant;
+    }
+
+    public void setWeeksPregnant(Integer weeksPregnant) {
+        this.weeksPregnant = weeksPregnant;
+    }
+
+    public String getPrescription1() {
+        return prescription1;
+    }
+
+    public void setPrescription1(String prescription1) {
+        this.prescription1 = prescription1;
+    }
+
+    public String getPrescription2() {
+        return prescription2;
+    }
+
+    public void setPrescription2(String prescription2) {
+        this.prescription2 = prescription2;
+    }
+
+    public String getPrescription3() {
+        return prescription3;
+    }
+
+    public void setPrescription3(String prescription3) {
+        this.prescription3 = prescription3;
+    }
+
+    public String getPrescription4() {
+        return prescription4;
+    }
+
+    public void setPrescription4(String prescription4) {
+        this.prescription4 = prescription4;
+    }
+
+    public String getPrescription5() {
+        return prescription5;
+    }
+
+    public void setPrescription5(String prescription5) {
+        this.prescription5 = prescription5;
+    }
+
+    public String getAssessment() {
+        return assessment;
+    }
+
+    public void setAssessment(String assessment) {
+        this.assessment = assessment;
+    }
+
+    public String getProblem1() {
+        return problem1;
+    }
+
+    public void setProblem1(String problem1) {
+        this.problem1 = problem1;
+    }
+
+    public String getProblem2() {
+        return problem2;
+    }
+
+    public void setProblem2(String problem2) {
+        this.problem2 = problem2;
+    }
+
+    public String getProblem3() {
+        return problem3;
+    }
+
+    public void setProblem3(String problem3) {
+        this.problem3 = problem3;
+    }
+
+    public String getProblem4() {
+        return problem4;
+    }
+
+    public void setProblem4(String problem4) {
+        this.problem4 = problem4;
+    }
+
+    public String getProblem5() {
+        return problem5;
+    }
+
+    public void setProblem5(String problem5) {
+        this.problem5 = problem5;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getOnset() {
+        return onset;
+    }
+
+    public void setOnset(String onset) {
+        this.onset = onset;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public String getRadiation() {
+        return radiation;
+    }
+
+    public void setRadiation(String radiation) {
+        this.radiation = radiation;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public String getProvokes() {
+        return provokes;
+    }
+
+    public void setProvokes(String provokes) {
+        this.provokes = provokes;
+    }
+
+    public String getPalliates() {
+        return palliates;
+    }
+
+    public void setPalliates(String palliates) {
+        this.palliates = palliates;
+    }
+
+    public String getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(String timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+    public String getPhysicalExamination() {
+        return physicalExamination;
+    }
+
+    public void setPhysicalExamination(String physicalExamination) {
+        this.physicalExamination = physicalExamination;
+    }
+
+    public Float getGlucose() {
+        return glucose;
+    }
+
+    public void setGlucose(Float glucose) {
+        this.glucose = glucose;
+    }
+
+    public String getNarrative() {
+        return narrative;
+    }
+
+    public void setNarrative(String narrative) {
+        this.narrative = narrative;
+    }
+    public String getCurrentMedication() {
+        return currentMedication;
+    }
+
+    public void setCurrentMedication(String currentMedication) {
+        this.currentMedication = currentMedication;
+    }
+
+    public String getSocialHistory() {
+        return socialHistory;
+    }
+
+    public void setSocialHistory(String socialHistory) {
+        this.socialHistory = socialHistory;
+    }
+
+    public String getMedicalSurgicalHistory() {
+        return medicalSurgicalHistory;
+    }
+
+    public void setMedicalSurgicalHistory(String medicalSurgicalHistory) {
+        this.medicalSurgicalHistory = medicalSurgicalHistory;
+    }
+
+    public String getFamilyHistory() {
+        return familyHistory;
+    }
+
+    public void setFamilyHistory(String familyHistory) {
+        this.familyHistory = familyHistory;
+    }
+
+    public String[] getMedications() { return medications; }
+
+    public void setMedications(String[] medications) { this.medications = medications; }
+    public String getMedication(int i) { return medications[i]; }
+
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { this.address = address;}
+
+    public String getFamilyHist() { return familyHist; }
+
+    public void setFamilyHist(String familyHist) { this.familyHist = familyHist; }
+
+    public List<String> getProblemList() { return problemList; }
+
+    public void setProblemList(List<String> problemList) { this.problemList = problemList; }
+
+    public List<String> getPerscribList() { return perscribList; }
+
+    public void setPerscribList(List<String> perscribList) { this.perscribList = perscribList; }
+
+
+
+
+    /* public Integer getBloodPressureSystolic() {
         return bloodPressureSystolic;
     }
 
@@ -212,5 +618,5 @@ public class CreateEncounterViewModel {
 
     public void setGlucose(Float glucose) {
         this.glucose = glucose;
-    }
+    }*/
 }

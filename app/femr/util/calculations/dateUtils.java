3689a8b33f7ba0ad537ab8b549e0d8ae00a77b0a
Cohen-Carlisle <cohen.carlisle@gmail.com>
2014-02-20 18:37:11 -0500

    Changed 'yo' (years old) to 'yoa' (years of age)

--- app/femr/util/calculations/dateUtils.java
+++ app/femr/util/calculations/dateUtils.java
@@ -35,7 +35,7 @@ public class dateUtils {
         if (monthsInt < 24)
             return Integer.toString(monthsInt) + " mo";
         else
-            return Integer.toString(monthsInt/12) + " yo";
+            return Integer.toString(monthsInt/12) + " yoa";
     }
 }
 

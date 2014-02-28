a33702b8b8091c537fc96a20171a7899636e00af
b6025 <xijiem@gmail.com>
2014-02-28 14:19:58 -0500

    Photo upload working

--- app/femr/business/services/IPhotoService.java
+++ app/femr/business/services/IPhotoService.java
@@ -1,14 +1,15 @@
 package femr.business.services;
 
 import java.io.*;
-import femr.business.dtos.ServiceResponse;
-import femr.common.models.*;
+
+import femr.common.models.IPatient;
 
 public interface IPhotoService {
 
 
     String GetPhotoPath();
-    void SavePhoto(File imgFile, String fileName);
-    void CropImage(File imgFile, String coords);
-    ServiceResponse<IPhoto> createPhoto(IPhoto photo);
+    //void SavePhoto(File imgFile, String fileName);
+    //void CropImage(File imgFile, String coords);
+    //ServiceResponse<IPhoto> createPhoto(IPhoto photo);
+    void HandlePatientPhoto(File img, IPatient patient, String coords, Boolean deleteFlag);
 }

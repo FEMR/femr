f0fc8f1b0fd53b7dd5e413a551e6e899cfd53bdb
codeitandloadit <reinhei2@wayne.edu>
2014-03-07 13:15:54 -0500

    Got a place holder webpage up.

--- app/femr/business/services/IResearchService.java
+++ app/femr/business/services/IResearchService.java
@@ -3,7 +3,11 @@ package femr.business.services;
  * This is the research service interface.
  */
 import femr.business.dtos.ServiceResponse;
+import femr.common.models.IPatient;
+
+import java.util.List;
 
 public interface IResearchService {
+    ServiceResponse<List<? extends IPatient>> findAllPatient();  // returns a list of all the patients so we can narrow it down
     //TODO-RESEARCH: Declare the services
 }

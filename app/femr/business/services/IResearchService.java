25250d97db700476541760c512753fe673ac6363
codeitandloadit <reinhei2@wayne.edu>
2014-03-06 21:04:45 -0500

    Created the initial Files for the Research feature

---
+++ app/femr/business/services/IResearchService.java
@@ -0,0 +1,9 @@
+package femr.business.services;
+/**
+ * This is the research service interface.
+ */
+import femr.business.dtos.ServiceResponse;
+
+public interface IResearchService {
+    //TODO-RESEARCH: Declare the services
+}
---
+++ app/femr/ui/models/research/CreateViewModelGet.java
@@ -0,0 +1,10 @@
+package femr.ui.models.research;
+
+/**
+ *  This defines all the objects being returned to the page in the response
+ */
+public class CreateViewModelGet {
+
+    //TODO-RESEARCH: Add all the objects that we will need for the page
+
+}

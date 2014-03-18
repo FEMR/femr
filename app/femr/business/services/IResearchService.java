07e46fe7a17515ea91eefc9a10b568a4113358cb
codeitandloadit <reinhei2@wayne.edu>
2014-03-18 14:39:04 -0400

    fix and updates

--- app/femr/business/services/IResearchService.java
+++ app/femr/business/services/IResearchService.java
@@ -1,12 +1,14 @@
 package femr.business.services;
-/**
- * This is the research service interface.
- */
+
+
 import femr.business.dtos.ServiceResponse;
 import femr.common.models.IPatient;
 
 import java.util.List;
 
+/**
+ * Interface for the Research Service
+ */
 public interface IResearchService {
     ServiceResponse<List<? extends IPatient>> findAllPatient();  // returns a list of all the patients so we can narrow it down
     //TODO-RESEARCH: Declare the services
--- app/femr/ui/models/research/CreateViewModelRequest.java
+++ app/femr/ui/models/research/CreateViewModelRequest.java
@@ -5,6 +5,18 @@ package femr.ui.models.research;
  */
 public class CreateViewModelRequest {
 
+    private QueryObjectPatientModel patientModel;
+    // TODO-RESEARCH: Create a medication model and add it here
+
+    public QueryObjectPatientModel getPatientModel() {
+        return patientModel;
+    }
+
+    public void setPatientModel(QueryObjectPatientModel patientModel) {
+        this.patientModel = patientModel;
+    }
+
+
     //TODO-RESEARCH: Add all the objects that we will need for the page
 
 }

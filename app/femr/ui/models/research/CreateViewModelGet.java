90519b3fa241359e2f2f3d587e8769b106b3f15d
codeitandloadit <reinhei2@wayne.edu>
2014-03-21 09:58:00 -0400

    update research model and controller

---
+++ app/femr/ui/models/research/CreateViewModelGet.java
@@ -0,0 +1,15 @@
+package femr.ui.models.research;
+
+/**
+ * this is the view model for the research page for sending the data back from the server
+ *
+ * this is the blank page !!! Update this description when done !!!
+ */
+public class CreateViewModelGet {
+
+
+
+
+
+    // TODO-RESEARCH: Create model for displaying the data
+}

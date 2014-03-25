2e5f1c662083c22c050c5a9b147a502fb746f717
codeitandloadit <reinhei2@wayne.edu>
2014-03-25 12:35:52 -0400

    created the data model

--- app/femr/business/services/IResearchService.java
+++ app/femr/business/services/IResearchService.java
@@ -3,6 +3,7 @@ package femr.business.services;
 
 import femr.business.dtos.ServiceResponse;
 import femr.common.models.IPatient;
+import femr.common.models.IPatientResearch;
 
 import java.util.List;
 
@@ -10,6 +11,8 @@ import java.util.List;
  * Interface for the Research Service
  */
 public interface IResearchService {
-    ServiceResponse<List<? extends IPatient>> findAllPatient();  // returns a list of all the patients so we can narrow it down
+   // ServiceResponse<List<? extends IPatient>> findAllPatient();  // returns a list of all the patients so we can narrow it down
     //TODO-RESEARCH: Declare the services
+
+    ServiceResponse<List<? extends IPatientResearch>> testModel();
 }

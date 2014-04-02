0594789b6970260d479238c4c2f687d5091bf0f2
Kevin Zurek <kevinzurek@gmail.com>
2014-04-01 22:14:05 -0400

    fixed up pharmacy javascript and typeahead
    remove extra comments
    
    fix auto complete

--- public/js/pharmacy/pharmacy.js
+++ public/js/pharmacy/pharmacy.js
@@ -1,12 +1,7 @@
 var meds;
 $(document).ready(function () {
-    $('.toggleReplacement').click(function () {
-        if ($(this).attr('id').substr(0, 1) == "y") {
-            yesReplacement($(this).attr('id').substr(1, 2));
-        }
-        else {
-            noReplacement($(this).attr('id').substr(1, 2));
-        }
+    $('.replaceBtn').click(function(){
+        replaceClick(this);
     });
 
     //get medications
@@ -15,21 +10,19 @@ $(document).ready(function () {
     });
 });
 
-function yesReplacement(id) {
-    $('#replacementMedication' + id).removeClass('hidden');
-    $('#replacementAmount' + id).removeClass('hidden');
-    //initalize typeahead
-    $('#replacementMedication' + id).typeahead({
-        name: 'medication',
-        local: meds
-    });
-}
 
-function noReplacement(id) {
-    $('#replacementMedication' + id).addClass('hidden');
-    $('#replacementAmount' + id).addClass('hidden');
-    $('#replacementMedication' + id).val('');
-    //remove typeahead while no button is clicked
-    $('#replacementMedication' + id).typeahead('destroy');
+function replaceClick(btn){
+    if ($(btn).next().hasClass('hidden')){
+        $(btn).next().removeClass('hidden');
+        //initalize typeahead
+        $(btn).next().typeahead({
+            name: 'medication',
+            local: meds
+        });
+    }else{
+        $(btn).next().addClass('hidden');
+        //eliminate typeahead
+        $(btn).next().typeahead('destroy');
+    }
 }
 

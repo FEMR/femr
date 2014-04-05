9304c6ac66878ea95ce64c3e3fccbd063ca53e42
Kevin Zurek <kevinzurek@gmail.com>
2014-04-05 07:25:23 -0400

    temporary vital fix

--- public/js/triage/triage.js
+++ public/js/triage/triage.js
@@ -157,6 +157,22 @@ $(document).ready(function () {
             _photoDeleteImage();
     });
     //PHOTO LOGIC ::END::  =~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+    (function () {
+        Date.prototype.toYMD = Date_toYMD;
+        function Date_toYMD() {
+            var year, month, day;
+            year = String(this.getFullYear());
+            month = String(this.getMonth() + 1);
+            if (month.length == 1) {
+                month = "0" + month;
+            }
+            day = String(this.getDate());
+            if (day.length == 1) {
+                day = "0" + day;
+            }
+            return year + "-" + month + "-" + day;
+        }
+    })();
 
 });
 

ac4acbbda96aa4c40102192dab2e49c876fc7363
Kevin Zurek <kevinzurek@gmail.com>
2014-10-12 18:30:12 -0400

    working on admin medication/inventory user interface

--- public/js/admin/inventory.js
+++ public/js/admin/inventory.js
@@ -1,28 +1,5 @@
 $(document).ready(function () {
-    $('#editDialog').dialog({
-        dialogClass: 'editUserDialog',
-        autoOpen: false,
-        draggable: true,
-        position: 'center',
-        modal: true,
-        height: 450,
-        width: 500
 
-    });
-
-    window.medicationsToAdd = [];
-
-    $('.remove').find('span').click(function(){
-        var name = $(this).parent().parent().find('.name').text();//.find('.name').text();
-        if (confirm("Are you sure you want to PERMANENTLY remove " + name + " from your inventory? No data will be kept on " + name + ".")){
-//            var id = $(this).find('input').val();
-            return true;
-        }else{
-            //do nothing
-            return false;
-        }
-
-    });
 
 
 });
@@ -37,21 +14,6 @@ function Medication(name, quantity) {
     this.quantity_total = quantity;
 }
 
-/*
- /**
- * populates current medications as they exist on the page
- *
- function populateAllCurrentMedications(){
- var medications = $('#inventoryTable tbody > tr');
- var medication;
- $(medications).each(function(){
- var name = $(this).find('.name').text();
- var totalQuantity = $(this).find('.totalQuantity').text();
- var currentQuantity = $(this).find('.currentQuantity').text();
- medication = new Medication(name, totalQuantity, currentQuantity);
- window.allMedications.push(medication);
- });
- } */
 
 //Medication.prototype = {
 //    //add and remove both interact directly with the database

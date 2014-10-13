46c5c489ae5d6d1328717c68f1a979492208a331
Kevin Zurek <kevinzurek@gmail.com>
2014-10-13 18:49:29 -0400

    working on inventory interface

--- public/js/admin/inventory.js
+++ public/js/admin/inventory.js
@@ -1,18 +1,46 @@
 $(document).ready(function () {
 
 
-
 });
 
+var inventoryFields = {
+    newMedications: {
+        quantity: $('#quantityInput'),
+        name: $('#nameInput'),
+        strength: $('#strengthInput'),
+        ingredient: $('#ingredientInput'),
+        unit: $('#unitInput'),
+        form: $('#formInput')
+    }
+
+};
+
+var medicationInventoryFeature = {
+    ingredientsList: $('#ingredientsForMedication'),
+    addIngredient: function () {
+
+    },
+    submitMedication: function () {
+
+    }
+
+};
 
 
 /*
  Defines a medication, matches MedicationItem
  */
-function Medication(name, quantity) {
+function Medication(name, quantity, form) {
     this.name = name;
     this.quantity_total = quantity;
+    this.form = form;
+
 }
+var ingredient = {
+    strength: null,
+    name: null,
+    unit: null
+};
 
 
 //Medication.prototype = {

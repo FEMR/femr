68d9b93100ab332242f93ebc0781f28405953040
Kevin Zurek <kevinzurek@gmail.com>
2014-11-04 18:46:09 -0500

    client validation

--- public/js/admin/inventory.js
+++ public/js/admin/inventory.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
-    $('#addNewIngredient').click(function(){
-        medicationInventoryFeature.addIngredient();
-    });
-
+    medicationInventoryFeature.bindAddNewIngredientButton();
+    medicationInventoryFeature.bindSubmitMedicationButton();
 });
 
 var inventoryFields = {
-    newMedications: {
+    newMedication: {
         quantity: $('#quantityFieldMedication').find('input'),
         form: $('#formFieldMedication').find('select'),
         name: $('#nameFieldMedication').find('input'),
-        strength: $('.strengthFieldMedication input'),
-        ingredient: $('.ingredientFieldMedication input'),
-        unit: $('.unitFieldMedication select')
+        strength: $('.strengthFieldMedication input:enabled'),
+        ingredient: $('.ingredientFieldMedication input:enabled'),
+        unit: $('.unitFieldMedication select:enabled'),
+        refreshFields: function(){
+            inventoryFields.newMedication.strength = $(inventoryFields.newMedication.strength.selector);
+            inventoryFields.newMedication.ingredient = $(inventoryFields.newMedication.ingredient.selector);
+            inventoryFields.newMedication.unit = $(inventoryFields.newMedication.unit.selector);
+        }
     }
 };
 
@@ -20,15 +23,41 @@ var medicationInventoryFeature = {
     newIngredientRow: $('.newIngredientRow'),
     addIngredient: function () {
         if (!medicationInventoryFeature.newIngredientRow.hasClass('hidden')){
-            //clone it
+            //clone the new ingredient div
             medicationInventoryFeature.newIngredientRow.clone().appendTo('#newMedicationWrap');
+            //since it cloned with values, clear the values
+            var newDiv = $('.newIngredientRow').last();
+            $(newDiv).find('.ingredientFieldMedication input').val("");
+            $(newDiv).find('.strengthFieldMedication input').val("");
         }else{
-            //show it
+            //show the new ingredient div
+            medicationInventoryFeature.newIngredientRow.find('.ingredientFieldMedication input').prop("disabled", false);
+            medicationInventoryFeature.newIngredientRow.find('.strengthFieldMedication input').prop("disabled", false);
+            medicationInventoryFeature.newIngredientRow.find('.unitFieldMedication select').prop("disabled", false);
             medicationInventoryFeature.newIngredientRow.removeClass('hidden');
         }
 
     },
-    submitMedication: function () {
-
+    validateMedication: function () {
+        var pass = true;
+        if (inventoryFields.newMedication.quantity.val() === "")
+            pass = false;
+        if (inventoryFields.newMedication.form.val() === null)
+            pass= false;
+        if (inventoryFields.newMedication.name.val() === "")
+            pass = false;
+        inventoryFields.newMedication.refreshFields();
+        console.log(inventoryFields.newMedication.strength.length);
+        return pass;
+    },
+    bindSubmitMedicationButton: function(){
+        $('#submitMedicationButton').click(function(){
+            return medicationInventoryFeature.validateMedication();
+        });
+    },
+    bindAddNewIngredientButton: function(){
+        $('#addNewIngredient').click(function(){
+            medicationInventoryFeature.addIngredient();
+        });
     }
 };

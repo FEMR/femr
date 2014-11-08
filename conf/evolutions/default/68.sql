e2330f8a4787a239be7b92ee009313ee620bf165
Kevin Zurek <kevinzurek@gmail.com>
2014-11-08 09:43:55 -0500

    adding a bean for age classification

---
+++ conf/evolutions/default/68.sql
@@ -0,0 +1,16 @@
+# --- !Ups
+
+CREATE TABLE `patient_age_classifications` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `isDeleted` BIT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC),
+  UNIQUE INDEX `description_UNIQUE` (`description` ASC));
+
+# --- !Downs
+
+DROP TABLE `patient_age_classifications`;
+

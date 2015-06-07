26394ca40a4d0f4ee3948c8bbeeb1fe6c99683f3
Kevin Zurek <kevinzurek@gmail.com>
2015-06-06 21:31:00 -0400

    updated version to 2.1.4 to prepare for merge into master

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Play.autoImport._
 object ApplicationBuild extends Build {
 
   val appName = "fEMR"
-  val appVersion = "2.1.3.3"
+  val appVersion = "2.1.4"
   val currentScalaVersion = "2.11.2"
 
   val appDependencies = Seq(

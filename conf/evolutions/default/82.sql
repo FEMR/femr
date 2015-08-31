2cf909b0c737b1a0b88087608a6c8ea2f7a25b38
Kevin Zurek <kevinzurek@gmail.com>
2015-08-31 18:30:20 -0400

    down evolution for 81 completed... in theory

--- conf/evolutions/default/82.sql
+++ conf/evolutions/default/82.sql
@@ -12,7 +12,7 @@ ALTER TABLE `patient_prescriptions`
 ADD COLUMN `replacement_id` INT(11) NULL DEFAULT NULL AFTER `date_taken`,
 ADD INDEX `fk_patient_prescriptions_replacement_id_patent_prescription_idx` (`replacement_id` ASC);
 ALTER TABLE `patient_prescriptions`
-ADD CONSTRAINT `fk_patient_prescriptions_replacement_id_patent_prescription_idx`
+ADD CONSTRAINT `fk_patient_prescriptions_replacement_id_patient_prescriptions_id`
 FOREIGN KEY (`replacement_id`)
 REFERENCES `patient_prescriptions` (`id`)
   ON DELETE RESTRICT

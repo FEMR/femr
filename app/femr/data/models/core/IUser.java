56270c43517b4d3ceb4f5cdfd7482596ee8b255f
AJ Saclayan <ajsaclayan@gmail.com>
2015-11-25 12:56:27 -0500

    Merged with development.  Merge IUser - AJ

--- app/femr/data/models/core/IUser.java
+++ app/femr/data/models/core/IUser.java
@@ -68,10 +68,8 @@ public interface IUser {
 
     void setNotes(String notes);
 
-<<<<<<< HEAD
     List<IMissionTrip> getMissionTrips();
 
     void setMissionTrips(List<IMissionTrip> missionTrips);
-=======
->>>>>>> 668946c... Added OriginalMedications to PDF Controller - AJ
+
 }

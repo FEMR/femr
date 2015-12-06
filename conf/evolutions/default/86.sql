e6458730db242258abd9fa719cc34435e7fecc34
Kevin Zurek <kevinzurek@gmail.com>
2015-12-05 19:39:42 -0500

    added missing grave symbol in evolution

--- conf/evolutions/default/86.sql
+++ conf/evolutions/default/86.sql
@@ -5,5 +5,5 @@ ADD COLUMN `isDeleted` DATETIME NULL DEFAULT NULL AFTER `photo_id`;
 
 # --- !Downs
 
-ALTER TABLE `patients`;
-DROP COLUMN `isDeleted;
+ALTER TABLE `patients`
+DROP COLUMN `isDeleted`;

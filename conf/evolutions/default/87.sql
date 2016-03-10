9973f61c8259e7084e4fa95731b15099265e87a9
Ayush Pawar <Ayush Pawar>
2016-03-10 17:40:40 -0500

    Changed the Phone No. field to optional and added sql 87 for evolution

--- conf/evolutions/default/87.sql
+++ conf/evolutions/default/87.sql
@@ -5,10 +5,18 @@ ADD COLUMN `deleted_by_user_id` INT(11) NULL DEFAULT NULL  AFTER `isDeleted`;
 ALTER TABLE `medication_inventories`
 ADD COLUMN `isDeleted` DATETIME NULL DEFAULT NULL AFTER `medication_id`;
 
+ALTER TABLE `patients`
+ADD COLUMN `phone_no` VARCHAR (255)DEFAULT NULL AFTER `photo_id`;
+
 # --- !Downs
 
 ALTER TABLE `patients`
 DROP COLUMN `deleted_by_user_id`;
+
 ALTER TABLE `medication_inventories`
 DROP COLUMN `isDeleted`;
+
+ALTER TABLE `patients`
+DROP COLUMN `phone_no`;
+
 

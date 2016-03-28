bd6edd6853bd458937de0e1d03601531cf1eec3e
Ayush Pawar <Ayush Pawar>
2016-03-28 12:29:40 -0400

    FEMR-147 Allow a user to search for a patient by phone number

--- app/femr/business/services/system/SearchService.java
+++ app/femr/business/services/system/SearchService.java
@@ -453,6 +453,7 @@ public class SearchService implements ISearchService {
         String firstName = null;
         String lastName = null;
         String firstOrLastName = null;
+        Integer phoneNo = null;
         if (words.length == 0) {
             //nothing was in the query
             response.addError("", "query string empty");
@@ -462,6 +463,7 @@ public class SearchService implements ISearchService {
             try {
                 //see if it is a number
                 id = Integer.parseInt(words[0]);
+                phoneNo = Integer.parseInt(words[0]);
             } catch (NumberFormatException ex) {
                 //see if it it a string
                 firstOrLastName = words[0];
@@ -483,7 +485,8 @@ public class SearchService implements ISearchService {
             //this is the most ideal scenario
             query = QueryProvider.getPatientQuery()
                     .where()
-                    .eq("id", id)
+                    .or(Expr.eq("id", id),
+                            Expr.eq("phone_no",phoneNo))
                     .isNull("isDeleted")
                     .order()
                     .desc("id");

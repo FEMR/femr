ddbd467c0da8812b9f93e5cab98755f71e8bffd9
Kevin Zurek <kevinzurek@gmail.com>
2016-04-02 13:08:20 -0400

    fix naming in evolution 96

--- conf/evolutions/default/96.sql
+++ conf/evolutions/default/96.sql
@@ -4,12 +4,12 @@ CREATE TABLE `concept_medications` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL DEFAULT NULL,
   `isDeleted` BIT(1) NULL DEFAULT b'0',
-  `concept_medication_forms_id` INT(11) UNSIGNED NULL DEFAULT NULL,
+  `concept_medication_form_id` INT(11) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_concept_medications_concept_medication_forms_idx` (`concept_medication_forms_id` ASC),
+  INDEX `fk_concept_medications_concept_medication_forms_idx` (`concept_medication_form_id` ASC),
   CONSTRAINT `fk_concept_medications_concept_medication_forms`
-    FOREIGN KEY (`concept_medication_forms_id`)
+    FOREIGN KEY (`concept_medication_form_id`)
     REFERENCES `concept_medication_forms` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
@@ -33,12 +33,12 @@ CREATE TABLE `concept_medication_generic_strengths` (
   INDEX `fk_concept_generic_strengths_concept_generics_idx` (`concept_medication_generic_id` ASC),
   CONSTRAINT `fk_concept_generic_strengths_concept_units`
   FOREIGN KEY (`concept_medication_unit_id`)
-  REFERENCES `concept_medication_unit` (`id`)
+  REFERENCES `concept_medication_units` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_concept_generic_strengths_concept_generics`
   FOREIGN KEY (`concept_medication_generic_id`)
-  REFERENCES `concept_medication_generic` (`id`)
+  REFERENCES `concept_medication_generics` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 

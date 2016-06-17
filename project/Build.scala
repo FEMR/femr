ebf94e5d46117d973fa57635b3922a312def26ce
Kevin Zurek <kevinzurek@gmail.com>
2016-06-17 19:25:57 -0400

    updated version to be 2.3.0-beta

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ import sbt._
 object ApplicationBuild extends Build {
 
   val appName = "fEMR"
-  val appVersion = "2.1.4"
+  val appVersion = "2.3.0-alpha"
   val currentScalaVersion = "2.11.7"
 
   val appDependencies = Seq(

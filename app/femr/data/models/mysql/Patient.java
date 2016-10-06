668742481248cb222689c47bae1c992dddfffc9d
ShaneQi <qizengtai@gmail.com>
2016-10-06 10:18:17 -0500

    Fixed code merging problem.

--- app/femr/data/models/mysql/Patient.java
+++ app/femr/data/models/mysql/Patient.java
@@ -62,9 +62,6 @@ public class Patient implements IPatient {
     @Column(name = "birthday_is_fake", nullable = true)
     private boolean birthdayIsFake;
 
-    @Column(name = "birthday_is_fake", nullable = true)
-    private boolean birthdayIsFake;
-
     @Override
     public int getId() {
         return id;

c0c0f087fc3fc36e037f4e123abbd17dbb3c4141
Melissa Heredia <fq2514@wayne.edu>
2016-10-20 20:23:16 -0400

    Commit view

--- app/femr/ui/controllers/admin/TriagePatientController.java
+++ app/femr/ui/controllers/admin/TriagePatientController.java
@@ -1,19 +1,30 @@
 package femr.ui.controllers.admin;
+import com.google.inject.Inject;
+import femr.business.services.core.ISessionService;
+import femr.common.dtos.CurrentUser;
 import femr.data.models.mysql.Roles;
 import femr.ui.helpers.security.AllowedRoles;
 import femr.ui.helpers.security.FEMRAuthenticated;
-import femr.ui.views.html.admin.triagePatient;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
-@Security.Authenticated(FEMRAuthenticated.class)
-@AllowedRoles({Roles.ADMINISTRATOR})
+import femr.ui.views.html.admin.triagePatients.triagePatient;
 
 /**
  * Created by fq251 on 10/20/2016.
  */
 
+@Security.Authenticated(FEMRAuthenticated.class)
+@AllowedRoles({Roles.PHYSICIAN, Roles.PHARMACIST, Roles.NURSE, Roles.ADMINISTRATOR})
 public class TriagePatientController extends Controller {
+
+    private final ISessionService sessionService;
+
+    @Inject
+    public TriagePatientController(ISessionService sessionService){
+        this.sessionService = sessionService;
+    }
+
    // public static Result triagePatientName()
        // {
         //return ok("Triage Patients");
@@ -21,7 +32,9 @@ public class TriagePatientController extends Controller {
     //}
     public  Result triagePatient()
     {
-        return ok(triagePatient.render());
+        CurrentUser currentUser = sessionService.retrieveCurrentUserSession();
+
+        return ok(triagePatient.render(currentUser));
         //return ok("Hello World!");
     }
 

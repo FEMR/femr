3930d75d866ffe666c818f42516c304d2d1ce6f3
Melissa Heredia <fq2514@wayne.edu>
2016-12-05 18:49:10 -0500

    Added manager table functionality

--- app/femr/ui/controllers/manager/ManagerController.java
+++ app/femr/ui/controllers/manager/ManagerController.java
@@ -1,10 +1,7 @@
 package femr.ui.controllers.manager;
 
 import com.google.inject.Inject;
-import femr.business.services.core.IEncounterService;
-import femr.business.services.core.IPatientService;
-import femr.business.services.core.ISearchService;
-import femr.business.services.core.ISessionService;
+import femr.business.services.core.*;
 import femr.common.dtos.CurrentUser;
 import femr.common.dtos.ServiceResponse;
 import femr.common.models.PatientEncounterItem;
@@ -23,6 +20,16 @@ import play.mvc.Security;
 import java.util.ArrayList;
 import java.util.List;
 
+import femr.common.models.TabFieldItem;
+import femr.ui.models.history.IndexEncounterMedicalViewModel;
+import femr.util.DataStructure.Mapping.TabFieldMultiMap;
+import femr.util.DataStructure.Mapping.VitalMultiMap;
+
+
+
+import java.util.HashMap;
+
+import java.util.Map;
 /**
  * Created by fq251 on 10/20/2016.
  */
@@ -35,15 +42,21 @@ public class ManagerController extends Controller {
     private final IPatientService patientService;
     private final IEncounterService encounterService;
     private final ISearchService searchService;
+    private final ITabService tabService;
+    private final IVitalService vitalService;
     private final Form<CreateViewModel> createViewModelForm = Form.form(CreateViewModel.class);
+
+
 
     @Inject
     public ManagerController(ISessionService sessionService, IPatientService patientService,
-                             IEncounterService encounterService, ISearchService searchService) {
+                             IEncounterService encounterService, ISearchService searchService,ITabService tabService, IVitalService vitalService) {
         this.sessionService = sessionService;
         this.patientService = patientService;
         this.encounterService=encounterService;
         this.searchService=searchService;
+        this.tabService = tabService;
+        this.vitalService = vitalService;
     }
 
     public Result indexGet() {
@@ -52,22 +65,53 @@ public class ManagerController extends Controller {
         CurrentUser currentUser = sessionService.retrieveCurrentUserSession();
         List<PatientItem> p=new ArrayList<PatientItem>();
         //returns a list of all triage patients
-        ServiceResponse<List<PatientItem>> allPatients= searchService.retrievePatientsForSearch(null);
-        //returns list of triage Patients on the current Day
-        ServiceResponse<List<PatientEncounterItem>> patientEncounterServices=encounterService.retrieveCurrentDayPatientEncounters();
-        //converts patient encounter Items to patient Items
-        for(int i=0;i<patientEncounterServices.getResponseObject().size();i++) {
-            ServiceResponse<PatientItem> translate = searchService.retrievePatientItemByPatientId(patientEncounterServices.getResponseObject().get(i).getPatientId());
-           PatientItem e=translate.getResponseObject();
-            p.add(e);
+        List<Map<String, TabFieldItem>> tab = new ArrayList<Map<String, TabFieldItem>>();
+        List<VitalMultiMap> vital = new ArrayList<VitalMultiMap>();
+        List<PatientEncounterItem> encounter = new ArrayList<PatientEncounterItem>();
+        IndexViewModelGet viewModel = new  IndexViewModelGet();
+        IndexEncounterMedicalViewModel hpimodel = new IndexEncounterMedicalViewModel();
+
+
+        if (currentUser.getTripId() == null) {
+            return ok(index.render(currentUser, viewModel, hpimodel));
         }
+        ServiceResponse<List<PatientEncounterItem>> patientEncounter = encounterService.retrieveCurrentDayPatientEncounters(currentUser.getTripId());
 
-        //sets Patients Items in view model used
-        IndexViewModelGet viewModel = new IndexViewModelGet();
+        //converts patient encounter Items to patient Items
+        for (int i = 0; i < patientEncounter.getResponseObject().size(); i++) {
+            ServiceResponse<PatientItem> translate = searchService.retrievePatientItemByPatientId(patientEncounter.getResponseObject()
+                    .get(i).getPatientId());
+            PatientItem e = translate.getResponseObject();
+            ServiceResponse<VitalMultiMap> patientEncounterVitalMapResponse = vitalService.retrieveVitalMultiMap
+                    (patientEncounter.getResponseObject().get(i).getId());
+            VitalMultiMap v = patientEncounterVitalMapResponse.getResponseObject();
+            //   hpimodel.getHpiFieldsWithMultipleChiefComplaints();
+            ServiceResponse<TabFieldMultiMap> patientEncounterTabFieldResponse = tabService.retrieveTabFieldMultiMap
+                    (patientEncounter.getResponseObject().get(i).getId());
+            TabFieldMultiMap tabFieldMultiMap = patientEncounterTabFieldResponse.getResponseObject();
+            Map<String, TabFieldItem> hpiFields = new HashMap<>();
+            if (patientEncounter.getResponseObject().get(i).getChiefComplaints().size() >= 1) {
+
+                for (String cc : patientEncounter.getResponseObject().get(i).getChiefComplaints()) {
+                    Map<String, TabFieldItem> hpiFieldsUnderChiefComplaint = new HashMap<>();
+                    hpiFieldsUnderChiefComplaint.put("narrative", tabFieldMultiMap.getMostRecentOrEmpty("narrative", cc.trim()));
+                    hpimodel.setHpiFieldsWithoutMultipleChiefComplaints(hpiFieldsUnderChiefComplaint);
+                    hpiFields.putAll(hpiFieldsUnderChiefComplaint);
+                }
+
+                tab.add(hpiFields);
+                vital.add(v);
+                p.add(e);
+            }
+        }
         viewModel.setTriagePatients(p);
+        viewModel.setPatientEncounter(patientEncounter.getResponseObject());
+        viewModel.setVitals(vital);
+        viewModel.setHPI(tab);
 
 
-        return ok(index.render(currentUser, viewModel));
+        return ok(index.render(currentUser, viewModel,hpimodel));
+
     }
 
 

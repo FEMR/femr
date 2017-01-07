7ee0aa62046022bce8a0669271a77dc98eeb1600
Kevin Zurek <kevinzurek@gmail.com>
2017-01-07 11:36:44 -0500

    add PhotoRepository classes and DI

---
+++ app/femr/data/daos/core/IPhotoRepository.java
@@ -0,0 +1,4 @@
+package femr.data.daos.core;
+
+public interface IPhotoRepository {
+}
---
+++ app/femr/data/daos/system/PhotoRepository.java
@@ -0,0 +1,6 @@
+package femr.data.daos.system;
+
+import femr.data.daos.core.IPhotoRepository;
+
+public class PhotoRepository implements IPhotoRepository {
+}

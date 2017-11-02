da6dd9e20714a4db790c1aee28c127f35bc6dfad
Mazen Saleh <Mazen@Mazens-MacBook-Pro.local>
2017-11-02 19:15:34 -0400

    Turn blood pressure text red & make it bigger if blood systolic blood pressure is >=140 diastolic 0

--- app/femr/ui/controllers/TriageController.java
+++ app/femr/ui/controllers/TriageController.java
@@ -297,7 +297,7 @@ public class TriageController extends Controller {
 
         return patient;
     }
-
+    //Mazen
     private List<String> parseChiefComplaintsJSON(String chiefComplaint, String chiefComplaintJSON) {
         List<String> chiefComplaints = new ArrayList<>();
         //JSON chief complaints (multiple chief complaints - requires javascript)

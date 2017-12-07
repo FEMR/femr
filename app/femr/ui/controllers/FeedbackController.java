3ac259105cb848849a7125b0e9d04a9f3e9447ba
Alex Manor <ajmanor@wayne.edu>
2017-12-06 20:46:18 -0500

    Simple Feedback Page

--- app/femr/ui/controllers/FeedbackController.java
+++ app/femr/ui/controllers/FeedbackController.java
@@ -3,10 +3,18 @@ package femr.ui.controllers;
 
 import com.google.inject.Inject;
 import femr.business.services.core.ISessionService;
+import femr.common.dtos.CurrentUser;
+import femr.data.models.mysql.Roles;
+import femr.ui.helpers.security.AllowedRoles;
+import femr.ui.helpers.security.FEMRAuthenticated;
 import play.mvc.Controller;
 import femr.ui.views.html.feedback.feedback;
 import play.mvc.Result;
+import play.mvc.Security;
 
+
+@Security.Authenticated(FEMRAuthenticated.class)
+@AllowedRoles({Roles.PHYSICIAN, Roles.PHARMACIST, Roles.NURSE})
 public class FeedbackController extends Controller {
 
     private final ISessionService sessionService;
@@ -17,11 +25,10 @@ public class FeedbackController extends Controller {
         this.sessionService = sessionService;
     }
 
-
     public Result indexGet() {
 
-        return ok(feedback.render(currentUser));
+        CurrentUser currentUser = sessionService.retrieveCurrentUserSession();
+
+        return ok(feedback.render());
     }
-
-
 }

6e1c077ee520ba3be1e24b77be8aedcbd6b0566f
Alex Manor <ajmanor@wayne.edu>
2017-12-10 10:43:28 -0500

    Changed Varchar size to 255 for comments

--- conf/evolutions/default/105.sql
+++ conf/evolutions/default/105.sql
@@ -2,8 +2,8 @@
 
 CREATE  TABLE `feedback` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `date` DATE NOT NULL,
-  `feedback` VARCHAR(MAX) NOT NULL,
+  `date` DATE NOT NULL ,
+  `feedback` VARCHAR(255) NOT NULL ,
   PRIMARY KEY (`id`) );
 
 # --- !Downs

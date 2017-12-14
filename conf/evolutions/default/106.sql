f8f48bcf037037adcf425342356d30bcf12b4f25
Kevin Zurek <kevinzurek@gmail.com>
2017-12-14 18:41:25 -0500

    [FEMR-335] Fix 106.sql evolution by adding explicit constraint for foreign key

--- conf/evolutions/default/106.sql
+++ conf/evolutions/default/106.sql
@@ -1,7 +1,8 @@
 # --- !Ups
 
 ALTER TABLE `medication_inventories`
-ADD FOREIGN KEY (`createdBy`)
+ADD CONSTRAINT `fk_created_by_user_id_user_id`
+FOREIGN KEY (`createdBy`)
 REFERENCES `users` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
@@ -9,4 +10,4 @@ REFERENCES `users` (`id`)
 # --- !Downs
 
 ALTER TABLE `medication_inventories`
-DROP FOREIGN KEY `createdBy`;
+DROP FOREIGN KEY `fk_created_by_user_id_user_id`;

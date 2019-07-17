0294c7c07f1f662297a42457d80709553cb58baf
Ken Dunlap <kdunlap4918@gmail.com>
2019-07-17 11:59:59 -0400

    Replaced Research with Export, added new views

--- app/femr/ui/controllers/ExportController.java
+++ app/femr/ui/controllers/ExportController.java
@@ -19,12 +19,16 @@
 package femr.ui.controllers;
 
 import com.google.inject.Inject;
+import controllers.AssetsFinder;
 import femr.business.services.core.IExportService;
+import femr.business.services.core.ISessionService;
+import femr.common.dtos.CurrentUser;
 import femr.common.dtos.ServiceResponse;
 import femr.data.models.mysql.Roles;
 import femr.ui.helpers.security.AllowedRoles;
 import femr.ui.helpers.security.FEMRAuthenticated;
 import femr.ui.models.export.FilterViewModel;
+import femr.ui.views.html.export.index;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Result;
@@ -39,15 +43,20 @@ import static play.mvc.Results.ok;
 @AllowedRoles({Roles.RESEARCHER})
 public class ExportController {
 
+    private final AssetsFinder assetsFinder;
     private final FormFactory formFactory;
+    private ISessionService sessionService;
     private IExportService exportService;
 
     /**
+     * @param assetsFinder
      * @param formFactory
      * @param exportService {@link IExportService}
      */
     @Inject
-    public ExportController(FormFactory formFactory, IExportService exportService) {
+    public ExportController(AssetsFinder assetsFinder, ISessionService sessionService, FormFactory formFactory, IExportService exportService) {
+        this.assetsFinder = assetsFinder;
+        this.sessionService = sessionService;
         this.formFactory = formFactory;
         this.exportService = exportService;
     }
@@ -55,17 +64,28 @@ public class ExportController {
     /**
      * Trigger an export of all encounters
      */
-    public Result exportAllGet() {
+    public Result indexGet() {
+
+        CurrentUser currentUserSession = sessionService.retrieveCurrentUserSession();
 
         final Form<FilterViewModel> filterViewModelForm = formFactory.form(FilterViewModel.class);
         FilterViewModel viewModel = filterViewModelForm.bindFromRequest().get();
 
         // TODO - validate that there are trip ids or date range is present
 
-        ServiceResponse<File> filterServiceResponse = exportService.exportAllEncounters(viewModel.getMissionTripIds());
-        File csvFile = filterServiceResponse.getResponseObject();
-        response().setHeader("Content-disposition", "attachment; filename=" + csvFile.getName());
+        // Do Export
+        if(viewModel.getMissionTripIds().size() > 0) {
 
-        return ok(csvFile).as("application/x-download");
+            ServiceResponse<File> filterServiceResponse = exportService.exportAllEncounters(viewModel.getMissionTripIds());
+            File csvFile = filterServiceResponse.getResponseObject();
+            response().setHeader("Content-disposition", "attachment; filename=" + csvFile.getName());
+
+            return ok(csvFile).as("application/x-download");
+        }
+        // show form
+        else {
+
+            return ok(index.render(currentUserSession, assetsFinder, viewModel));
+        }
     }
 }
--- app/femr/ui/models/export/FilterViewModel.java
+++ app/femr/ui/models/export/FilterViewModel.java
@@ -1,10 +1,11 @@
 package femr.ui.models.export;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FilterViewModel {
 
-    private List<Integer> missionTripIds;
+    private List<Integer> missionTripIds = new ArrayList<>();
 
     public void setMissionTripIds(List<Integer> missionTripIds) {
         this.missionTripIds = missionTripIds;

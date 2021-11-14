9043a4c0a824e2e5ddd6d7efacb406eb51b7a7dc
Kylene Landenberger <62404695+kylene-phillips@users.noreply.github.com>
2021-11-13 17:14:48 -0800

    Added the Trauma sheet page, route, and controller (yet to be used), pair programming w Bat

---
+++ app/femr/ui/controllers/TraumaController.java
@@ -0,0 +1,40 @@
+package femr.ui.controllers;
+
+import com.google.inject.Inject;
+import controllers.AssetsFinder;
+import femr.business.services.core.ISessionService;
+import femr.common.dtos.CurrentUser;
+import femr.ui.models.trauma.IndexSheetView;
+import femr.ui.models.triage.IndexViewModelGet;
+import femr.ui.views.html.trauma.index; // this is what was needed to render the new page
+//import femr.ui.views.html.home.index;
+import femr.ui.views.html.sessions.create;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class TraumaController extends Controller {
+
+    private final AssetsFinder assetsFinder;
+    private ISessionService sessionService;
+
+    @Inject
+    public TraumaController(AssetsFinder assetsFinder, ISessionService sessionService) {
+
+        this.assetsFinder = assetsFinder;
+        this.sessionService = sessionService;
+    }
+
+    public Result index() {
+        CurrentUser currentUser = sessionService.retrieveCurrentUserSession();
+
+        if (currentUser != null) {
+            return ok(index.render(currentUser, assetsFinder));
+        }
+
+        return ok(create.render(null, 0, assetsFinder));
+    }
+
+
+
+
+}
---
+++ app/femr/ui/models/trauma/IndexSheetView.java
@@ -0,0 +1,5 @@
+package femr.ui.models.trauma;
+
+public class IndexSheetView {
+    // nothing yet
+}

5ee5fff7d30dcecc69268aa1ce1aadc5d2962cc7
Bat Battur <b.bagy@yahoo.com>
2022-02-15 09:36:08 -0800

    Added react-konva to the page.
    Added the library and other js files in separate folder.

---
+++ public/js/trauma/konvaDraw.js
@@ -0,0 +1,55 @@
+var width = window.innerWidth;
+var height = window.innerHeight - 25;
+
+// first we need Konva core things: stage and layer
+var stage = new Konva.Stage({
+    container: 'container',
+    width: width,
+    height: height,
+});
+
+var layer = new Konva.Layer();
+stage.add(layer);
+
+var isPaint = false;
+var mode = 'brush';
+var lastLine;
+
+stage.on('mousedown touchstart', function (e) {
+    isPaint = true;
+    var pos = stage.getPointerPosition();
+    lastLine = new Konva.Line({
+        stroke: '#df4b26',
+        strokeWidth: 5,
+        globalCompositeOperation:
+            mode === 'brush' ? 'source-over' : 'destination-out',
+        // round cap for smoother lines
+        lineCap: 'round',
+        // add point twice, so we have some drawings even on a simple click
+        points: [pos.x, pos.y, pos.x, pos.y],
+    });
+    layer.add(lastLine);
+});
+
+stage.on('mouseup touchend', function () {
+    isPaint = false;
+});
+
+// and core function - drawing
+stage.on('mousemove touchmove', function (e) {
+    if (!isPaint) {
+        return;
+    }
+
+    // prevent scrolling on touch devices
+    e.evt.preventDefault();
+
+    const pos = stage.getPointerPosition();
+    var newPoints = lastLine.points().concat([pos.x, pos.y]);
+    lastLine.points(newPoints);
+});
+
+var select = document.getElementById('tool');
+select.addEventListener('change', function () {
+    mode = select.value;
+});

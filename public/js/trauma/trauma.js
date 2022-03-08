3946faeae56f0e5ac8320c22028b1bb7480a166f
Bat Battur <b.bagy@yahoo.com>
2022-03-08 10:31:12 -0800

    Using javascript, select only one of the checkboxes.
    If female checkbox is selected, then enable the pregnancy input.
    Otherwise, disable if male or other is selected.

--- public/js/trauma/trauma.js
+++ public/js/trauma/trauma.js
@@ -10,3 +10,23 @@ function onlyOneTool(checkbox) {
         if (item !== checkbox) item.checked = false
     })
 }
+
+function onlyOneAge(checkbox) {
+    let checkboxes = [...document.getElementsByClassName('ageCheckbox')]
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function onlyOneGender(checkbox) {
+    let checkboxes = [...document.getElementsByClassName('genderCheckbox')]
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function checkPregnancyInput() {
+    let femaleCheckbox = document.getElementById("femaleCheckbox");
+    let weeksPregnant = document.getElementById("weeksPregnant");
+    femaleCheckbox.checked === true ? (weeksPregnant.disabled = false) : (weeksPregnant.disabled = true);
+}

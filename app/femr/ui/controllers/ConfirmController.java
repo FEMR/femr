cc48d4a3935fc943455cd10102d6bc564b59c6b2
Rey Ortiz <59678123+17rey17@users.noreply.github.com>
2022-03-13 16:02:35 -0700

    Implement submit patient page
    Implement the page once a patient is submitted

---
+++ app/femr/ui/controllers/ConfirmController.java
@@ -0,0 +1,35 @@
+package femr.ui.controllers;
+
+import com.google.inject.Inject;
+import controllers.AssetsFinder;
+import femr.business.services.core.ISessionService;
+import femr.common.dtos.CurrentUser;
+import femr.ui.models.trauma.IndexSheetView;
+import femr.ui.models.triage.IndexViewModelGet;
+import femr.ui.views.html.confirm.index; // this is what was needed to render the new page
+import femr.ui.views.html.sessions.create;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ConfirmController extends Controller {
+
+    private final AssetsFinder assetsFinder;
+    private ISessionService sessionService;
+
+    @Inject
+    public ConfirmController(AssetsFinder assetsFinder, ISessionService sessionService) {
+
+        this.assetsFinder = assetsFinder;
+        this.sessionService = sessionService;
+    }
+
+    public Result index() {
+        CurrentUser currentUser = sessionService.retrieveCurrentUserSession();
+
+        if (currentUser != null) {
+            return ok(index.render(currentUser, assetsFinder));
+        }
+
+        return ok(create.render(null, 0, assetsFinder));
+    }
+}

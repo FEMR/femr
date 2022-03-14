cfb03b769a5ca21a86276c34d2d2d5d86cb22fa0
Rey Ortiz <59678123+17rey17@users.noreply.github.com>
2022-03-13 19:13:32 -0700

    Saving image locally implementation
    Images can now be saved locally

--- public/js/trauma/trauma.js
+++ public/js/trauma/trauma.js
@@ -10,3 +10,22 @@ function onlyOne(checkbox) {
         if (item !== checkbox) item.checked = false
     })
 }
+
+function downloadURI(uri, name) {
+    var link = document.createElement('a');
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+}
+
+document.getElementById('save').addEventListener(
+    'click',
+    function () {
+        var dataURL = stage.toDataURL();
+        downloadURI(dataURL, 'patient.png');
+    },
+    false
+);

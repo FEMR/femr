d11a686cece281f256c19abb732a83c071fece0b
SeanHershey <shershey@calpoly.edu>
2023-11-16 09:29:15 -0800

    bug fix to translator

--- translator/translate.py
+++ translator/translate.py
@@ -1,11 +1,12 @@
 import sys
-import argostranslate.package
-import argostranslate.translate
+from argostranslate import package, translate
 
 def main():
-    if ( len(argostranslate.package.get_installed_packages()) == 0 ):
-        argostranslate.package.install_from_path('en_es.argosmodel')
-    text = argostranslate.translate.translate(sys.argv[1], 'en', 'es')
+    if ( len(package.get_installed_packages()) == 0 ):
+        package.install_from_path('en_es.argosmodel')
+    installed_languages = translate.get_installed_languages()
+    translation = installed_languages[0].get_translation(installed_languages[1])
+    text = translation.translate(sys.argv[1])
     print(list(text.encode('utf-8')))
 
 if __name__ == "__main__":

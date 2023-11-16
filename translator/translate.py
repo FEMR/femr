fb60ce4012db8f45f1ad588e3a5c4291738914dd
SeanHershey <shershey@calpoly.edu>
2023-11-16 09:57:19 -0800

    translation improvments

--- translator/translate.py
+++ translator/translate.py
@@ -1,13 +1,23 @@
 import sys
-from argostranslate import package, translate
+import argostranslate.package
+import argostranslate.translate
 
 def main():
-    if ( len(package.get_installed_packages()) == 0 ):
-        package.install_from_path('en_es.argosmodel')
-    installed_languages = translate.get_installed_languages()
-    translation = installed_languages[0].get_translation(installed_languages[1])
-    text = translation.translate(sys.argv[1])
-    print(list(text.encode('utf-8')))
+    from_code = "en"
+    to_code = "es"
+
+    if (len(argostranslate.package.get_installed_packages()) == 0):
+        argostranslate.package.update_package_index()
+        available_packages = argostranslate.package.get_available_packages()
+        package_to_install = next(
+            filter(
+                lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+            )
+        )
+        argostranslate.package.install_from_path(package_to_install.download())
+
+    translatedText = argostranslate.translate.translate(sys.argv[1], from_code, to_code)
+    print(list(translatedText.encode('utf-8')))
 
 if __name__ == "__main__":
    main()

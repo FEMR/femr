9bbd83f7127000338a674ba0a8154adfc291d3e9
jasonbarba <jrbarba@calpoly.edu>
2024-05-30 07:22:02 -0700

    Settings Page working with dropdown

--- app/femr/ui/controllers/SettingsController.java
+++ app/femr/ui/controllers/SettingsController.java
@@ -4,8 +4,6 @@ import com.google.inject.Inject;
 import controllers.AssetsFinder;
 import femr.business.services.core.ISessionService;
 import femr.common.dtos.CurrentUser;
-import femr.ui.views.html.home.index;
-import femr.ui.views.html.sessions.create;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -22,11 +20,6 @@ public class SettingsController extends Controller {
 
     public Result index() {
         CurrentUser currentUser = sessionService.retrieveCurrentUserSession();
-
-        if (currentUser != null) {
-            return ok(index.render(currentUser, assetsFinder));
-        }
-
-        return ok(create.render(null, 0, assetsFinder));
+        return ok(femr.ui.views.html.settings.index.render(currentUser, assetsFinder));
     }
 }

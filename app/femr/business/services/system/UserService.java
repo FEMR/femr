8f5ee9ff347c269862727a1ed49f656b97a83554
connor-addison-h <70348761+connor-addison-h@users.noreply.github.com>
2024-05-30 08:12:50 -0700

    updated userService to include language code

--- app/femr/business/services/system/UserService.java
+++ app/femr/business/services/system/UserService.java
@@ -241,6 +241,7 @@ public class UserService implements IUserService {
             user.setRoles(newRoles);
             user.setPasswordReset(userItem.isPasswordReset());
             user.setPasswordCreatedDate(DateTime.now());
+            user.setLanguageCode(userItem.getLanguageCode());
             user = userRepository.updateUser(user);
             response.setResponseObject(itemModelMapper.createUserItem(user));
         } catch (Exception ex) {

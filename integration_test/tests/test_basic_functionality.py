ef3d1d664d7b21389a041a76f7caf428498c9211
Dylan Starink <dylan@starink.com>
2025-04-17 08:28:31 -0700

    add back in chrome preferences

--- integration_test/tests/test_basic_functionality.py
+++ integration_test/tests/test_basic_functionality.py
@@ -15,15 +15,23 @@ import requests
 
 @pytest.fixture
 def driver():
+    chrome_prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.password_manager_leak_detection": False
+    }
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", chrome_prefs)
+    options.add_argument("--disable-dev-shm-usage")
+
     if os.getenv("USE_REMOTE"):
         driver_address = os.getenv("SELENIUM_ADDRESS")
         assert driver_address is not None, "SELENIUM_ADDRESS environment variable not set"
-        options = webdriver.ChromeOptions()
-        options.add_argument("--disable-dev-shm-usage")
         drvr = webdriver.Remote(command_executor=driver_address, options=options)
     else:
-        drvr = webdriver.Chrome()
-    drvr.implicitly_wait(30)
+        drvr = webdriver.Chrome(options=options)
+
     yield drvr
     drvr.quit()
 
@@ -448,19 +456,3 @@ def test_medical(driver):
     assert "urgent medical stuff" in driver.find_element(By.ID, "narrativeTab").get_attribute("value")
     assert "urgent, very important, medical stuff" in driver.find_element(By.ID, "physicalTab").get_attribute("value")
     assert "urgent medical stuff" in driver.find_element(By.ID, "palliatesTab").get_attribute("value")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
